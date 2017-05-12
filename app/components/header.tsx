59c2f872c327b1316749f7ea9e06b39a7b1d1b22
Logan Hasson <logan.hasson@gmail.com>
2017-05-12 19:01:24 -0400

    Fix title

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import * as styles from '../css/components/header.css';
 
 const Header = () => (
   <div className={styles.header}>
-    <Link className={styles.link} to='/'>Magma</Link>
+    <Link className={styles.link} to='/'>Commons</Link>
   </div>
 );
 

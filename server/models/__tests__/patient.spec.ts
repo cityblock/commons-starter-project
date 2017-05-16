b363c102ddf5a1d858cc64f39e8d3bd7813692b2
Brennan Moore <brennanmoore@gmail.com>
2017-05-16 17:47:36 -0400

    add patient spec

---
+++ server/models/__tests__/patient.spec.ts
@@ -0,0 +1,56 @@
+import Db from '../../db';
+import Patient from '../patient';
+
+describe('patient model', () => {
+  let db: Db = null as any;
+
+  beforeEach(async () => {
+    db = await Db.get();
+    await Db.clear();
+  });
+
+  afterAll(async () => {
+    await Db.release();
+  });
+
+  it('should create and retrieve a patient', async () => {
+    const patient = await Patient.create({ athenaPatientId: 123, firstName: 'a' lastName: 'b' });
+    expect(patient).toMatchObject({
+      id: patient.id,
+      athenaPatientId: 123,
+    });
+    const patientById = await Patient.get(patient.id);
+    expect(patientById).toMatchObject({
+      id: patient.id,
+      athenaPatientId: 123,
+    });
+  });
+
+  describe('patients', () => {
+    beforeEach(async () => {
+      await Patient.create({ athenaPatientId: 123, firstName: 'a', lastName: 'b' });
+      await Patient.create({ athenaPatientId: 234, firstName: 'c', lastName: 'd' });
+    });
+
+    it('should fetch patients', async () => {
+      expect(await Patient.getAll({ limit: 100 })).toMatchObject([
+        {
+          athenaPatientId: 123,
+        },
+        {
+          athenaPatientId: 234,
+        },
+      ]);
+    });
+
+    it('should fetch a limited set of patients', async () => {
+      expect(await Patient.getAll({ limit: 1 })).toMatchObject([
+        { athenaPatientId: 123 },
+      ]);
+      expect(await Patient.getAll({ limit: 1, offset: 1 })).toMatchObject([
+        { athenaPatientId: 234 },
+      ]);
+    });
+
+  });
+});

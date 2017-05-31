a91e7bed81578057c16c1648e0689c3fd6c95713
Brennan Moore <brennanmoore@gmail.com>
2017-05-31 12:44:06 -0400

    add fonts (#81)

--- webpack/rules/image.js
+++ webpack/rules/image.js
@@ -1,6 +1,6 @@
 const PATHS = require("../paths");
 
-module.exports = ({ limit = 10000 } = {}) => ({
+module.exports = ({ limit = 30000 } = {}) => ({
   test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
   loader: "url-loader",
   options: { name: "[hash].[ext]", limit },

2009ffdff58b2d72983efec97bc7af487ac25759
Brennan Moore <brennanmoore@gmail.com>
2017-06-19 16:58:28 -0400

    fix prod build

--- declarations/jest.d.ts
+++
@@ -1,7 +0,0 @@
-// Definitely typed hasn't been updated for jest 20.0.0+ yet, so we're missing some fancy matchers
-declare namespace jest {
-  interface Matchers {
-    resolves: Matchers;
-    rejects: Matchers;
-  }
-}

6313d5ac1efbeedda1e747db9df45e1d59ef125a
Brennan Moore <brennanmoore@gmail.com>
2017-07-10 15:17:21 -0400

    update apollo-codegen (#193)

--- app/graphql/types.ts
+++ app/graphql/types.ts
@@ -1,5 +1,5 @@
-//  This file was automatically generated and should not be edited.
 /* tslint:disable */
+//  This file was automatically generated and should not be edited.
 
 export type AppointmentStatus =
   "cancelled" |
@@ -19,294 +19,435 @@ export type UserRole =
   "admin";
 
 
-export interface AppointmentEndMutationVariables {
-  patientId: string;
-  appointmentId: string;
-  appointmentNote: string;
-}
+export type AppointmentEndMutationVariables = {
+  patientId: string,
+  appointmentId: string,
+  appointmentNote: string,
+};
 
-export interface AppointmentEndMutation {
+export type AppointmentEndMutation = {
   // End an appointment
-  appointmentEnd: {
+  appointmentEnd:  {
     success: boolean,
-  } | null;
-}
+  } | null,
+};
 
-export interface AppointmentStartMutationVariables {
-  patientId: string;
-}
+export type AppointmentStartMutationVariables = {
+  patientId: string,
+};
 
-export interface AppointmentStartMutation {
+export type AppointmentStartMutation = {
   // Start an appointment
-  appointmentStart: FullAppointmentFragment;
-}
+  appointmentStart:  {
+    athenaAppointmentId: string,
+    dateTime: string,
+    athenaDepartmentId: number,
+    status: AppointmentStatus,
+    athenaPatientId: number,
+    duration: number,
+    appointmentTypeId: number,
+    appointmentType: string,
+    athenaProviderId: number,
+    userId: string,
+    patientId: string,
+    clinicId: string,
+  } | null,
+};
 
-export interface GetClinicsQueryVariables {
-  pageNumber: number | null;
-  pageSize: number | null;
-}
+export type GetClinicsQueryVariables = {
+  pageNumber?: number | null,
+  pageSize?: number | null,
+};
 
-export interface GetClinicsQuery {
+export type GetClinicsQuery = {
   // Clinics
-  clinics: {
-    edges: Array< {
-      node: FullClinicFragment,
+  clinics:  {
+    edges:  Array< {
+      node:  {
+        id: string,
+        name: string,
+      } | null,
     } > | null,
-  } | null;
-}
+  } | null,
+};
 
-export interface CurrentUserEditMutationVariables {
-  firstName: string | null;
-  lastName: string | null;
-  locale: string | null;
-}
+export type CurrentUserEditMutationVariables = {
+  firstName?: string | null,
+  lastName?: string | null,
+  locale?: string | null,
+};
 
-export interface CurrentUserEditMutation {
+export type CurrentUserEditMutation = {
   // Edit current user
-  currentUserEdit: FullUserFragment;
-}
+  currentUserEdit:  {
+    id: string,
+    locale: string | null,
+    firstName: string | null,
+    lastName: string | null,
+    userRole: UserRole,
+    email: string | null,
+    homeClinicId: string,
+    googleProfileImageUrl: string | null,
+  } | null,
+};
 
-export interface GetCurrentUserQuery {
+export type GetCurrentUserQuery = {
   // The current User
-  currentUser: FullUserFragment;
-}
+  currentUser:  {
+    id: string,
+    locale: string | null,
+    firstName: string | null,
+    lastName: string | null,
+    userRole: UserRole,
+    email: string | null,
+    homeClinicId: string,
+    googleProfileImageUrl: string | null,
+  } | null,
+};
 
-export interface GetPatientCareTeamQueryVariables {
-  patientId: string;
-}
+export type GetPatientCareTeamQueryVariables = {
+  patientId: string,
+};
 
-export interface GetPatientCareTeamQuery {
+export type GetPatientCareTeamQuery = {
   // Users on a care team
-  patientCareTeam: Array<FullUserFragment>;
-}
+  patientCareTeam:  Array< {
+    id: string,
+    locale: string | null,
+    firstName: string | null,
+    lastName: string | null,
+    userRole: UserRole,
+    email: string | null,
+    homeClinicId: string,
+    googleProfileImageUrl: string | null,
+  } > | null,
+};
 
-export interface GetPatientEncountersQueryVariables {
-  patientId: string;
-}
+export type GetPatientEncountersQueryVariables = {
+  patientId: string,
+};
 
-export interface GetPatientEncountersQuery {
+export type GetPatientEncountersQuery = {
   // Patient encounters
-  patientEncounters: Array<FullPatientEncounterFragment>;
-}
+  patientEncounters:  Array< {
+    encounterType: string,
+    providerName: string,
+    providerRole: string,
+    location: string,
+    diagnoses:  Array< {
+      code: string,
+      codeSystem: string,
+      description: string,
+    } >,
+    reasons: Array< string | null >,
+    dateTime: string,
+  } > | null,
+};
 
-export interface GetPatientMedicationsQueryVariables {
-  patientId: string;
-}
+export type GetPatientMedicationsQueryVariables = {
+  patientId: string,
+};
 
-export interface GetPatientMedicationsQuery {
+export type GetPatientMedicationsQuery = {
   // Patient medications
-  patientMedications: {
-    medications: {
-      active: Array<FullPatientMedicationFragment>,
+  patientMedications:  {
+    medications:  {
+      active:  Array< {
+        medicationId: string,
+        name: string,
+        quantity: string | null,
+        quantityUnit: string | null,
+        dosageInstructions: string | null,
+        startDate: string | null,
+      } >,
     },
-  } | null;
-}
+  } | null,
+};
 
-export interface GetPatientPanelQueryVariables {
-  pageNumber: number | null;
-  pageSize: number | null;
-}
+export type GetPatientPanelQueryVariables = {
+  pageNumber?: number | null,
+  pageSize?: number | null,
+};
 
-export interface GetPatientPanelQuery {
+export type GetPatientPanelQuery = {
   // List of patients the user is on the care team for (their 'patient panel')
-  userPatientPanel: {
-    edges: Array< {
-      node: ShortPatientFragment,
+  userPatientPanel:  {
+    edges:  Array< {
+      node:  {
+        id: string,
+        firstName: string | null,
+        middleName: string | null,
+        lastName: string | null,
+        language: string | null,
+        gender: string | null,
+        dateOfBirth: string | null,
+        zip: number | null,
+        createdAt: string | null,
+        consentToText: boolean | null,
+        consentToCall: boolean | null,
+      } | null,
     } > | null,
-    pageInfo: {
+    pageInfo:  {
       hasPreviousPage: boolean,
       hasNextPage: boolean,
     },
-  } | null;
-}
+  } | null,
+};
 
-export interface GetPatientScratchPadQueryVariables {
-  patientId: string;
-}
+export type GetPatientScratchPadQueryVariables = {
+  patientId: string,
+};
 
-export interface GetPatientScratchPadQuery {
+export type GetPatientScratchPadQuery = {
   // Patient scratch pad
-  patientScratchPad: FullPatientScratchPadFragment;
-}
+  patientScratchPad:  {
+    text: string | null,
+  } | null,
+};
 
-export interface GetPatientQueryVariables {
-  patientId: string;
-}
+export type GetPatientQueryVariables = {
+  patientId: string,
+};
 
-export interface GetPatientQuery {
+export type GetPatientQuery = {
   // A single Patient
-  patient: ShortPatientFragment;
-}
+  patient:  {
+    id: string,
+    firstName: string | null,
+    middleName: string | null,
+    lastName: string | null,
+    language: string | null,
+    gender: string | null,
+    dateOfBirth: string | null,
+    zip: number | null,
+    createdAt: string | null,
+    consentToText: boolean | null,
+    consentToCall: boolean | null,
+  } | null,
+};
 
-export interface LogInUserMutationVariables {
-  googleAuthCode: string;
-}
+export type LogInUserMutationVariables = {
+  googleAuthCode: string,
+};
 
-export interface LogInUserMutation {
+export type LogInUserMutation = {
   // Login user
-  userLogin: {
+  userLogin:  {
     // The auth token to allow for quick login. JWT passed back in via headers for further requests
     authToken: string | null,
-    user: FullUserFragment,
-  } | null;
-}
+    user:  {
+      id: string,
+      locale: string | null,
+      firstName: string | null,
+      lastName: string | null,
+      userRole: UserRole,
+      email: string | null,
+      homeClinicId: string,
+      googleProfileImageUrl: string | null,
+    },
+  } | null,
+};
 
-export interface PatientEditMutationVariables {
-  patientId: string;
-  firstName: string | null;
-  middleName: string | null;
-  lastName: string | null;
-  dateOfBirth: string | null;
-  gender: string | null;
-  zip: number | null;
-  language: string | null;
-  consentToCall: boolean | null;
-  consentToText: boolean | null;
-}
+export type PatientEditMutationVariables = {
+  patientId: string,
+  firstName?: string | null,
+  middleName?: string | null,
+  lastName?: string | null,
+  dateOfBirth?: string | null,
+  gender?: string | null,
+  zip?: number | null,
+  language?: string | null,
+  consentToCall?: boolean | null,
+  consentToText?: boolean | null,
+};
 
-export interface PatientEditMutation {
+export type PatientEditMutation = {
   // Edit fields on patient stored in the db
-  patientEdit: ShortPatientFragment;
-}
+  patientEdit:  {
+    id: string,
+    firstName: string | null,
+    middleName: string | null,
+    lastName: string | null,
+    language: string | null,
+    gender: string | null,
+    dateOfBirth: string | null,
+    zip: number | null,
+    createdAt: string | null,
+    consentToText: boolean | null,
+    consentToCall: boolean | null,
+  } | null,
+};
 
-export interface PatientHealthRecordQueryVariables {
-  patientId: string;
-}
+export type PatientHealthRecordQueryVariables = {
+  patientId: string,
+};
 
-export interface PatientHealthRecordQuery {
+export type PatientHealthRecordQuery = {
   // Patient's Athena health record
-  patientHealthRecord: FullPatientHealthRecordFragment;
-}
+  patientHealthRecord:  {
+    id: string,
+    firstName: string | null,
+    lastName: string | null,
+    dateOfBirth: string | null,
+    gender: string | null,
+    suffix: string | null,
+    preferredName: string | null,
+    raceName: string | null,
+    race: string | null,
+    status: string | null,
+    ssn: string | null,
+    homebound: boolean | null,
+    language6392code: string | null,
+    maritalStatus: string | null,
+    maritalStatusName: string | null,
+    patientPhoto: boolean | null,
+    patientPhotoUrl: string | null,
+  } | null,
+};
 
-export interface PatientScratchPadEditMutationVariables {
-  patientId: string;
-  text: string;
-}
+export type PatientScratchPadEditMutationVariables = {
+  patientId: string,
+  text: string,
+};
 
-export interface PatientScratchPadEditMutation {
+export type PatientScratchPadEditMutation = {
   // Edit a patient scratch pad
-  patientScratchPadEdit: FullPatientScratchPadFragment;
-}
+  patientScratchPadEdit:  {
+    text: string | null,
+  } | null,
+};
 
-export interface PatientSetupMutationVariables {
-  homeClinicId: string;
-  firstName: string;
-  middleName: string | null;
-  lastName: string;
-  dateOfBirth: string;
-  gender: string;
-  maritalStatus: string;
-  race: string;
-  zip: number;
-  ssn: string;
-  language: string;
-  email: string | null;
-  homePhone: string | null;
-  mobilePhone: string | null;
-  consentToCall: boolean;
-  consentToText: boolean;
-  issueDate: string | null;
-  expirationDate: string | null;
-  insuranceType: string | null;
-  patientRelationshipToPolicyHolder: string | null;
-  memberId: string | null;
-  policyGroupNumber: string | null;
-}
+export type PatientSetupMutationVariables = {
+  homeClinicId: string,
+  firstName: string,
+  middleName?: string | null,
+  lastName: string,
+  dateOfBirth: string,
+  gender: string,
+  maritalStatus: string,
+  race: string,
+  zip: number,
+  ssn: string,
+  language: string,
+  email?: string | null,
+  homePhone?: string | null,
+  mobilePhone?: string | null,
+  consentToCall: boolean,
+  consentToText: boolean,
+  issueDate?: string | null,
+  expirationDate?: string | null,
+  insuranceType?: string | null,
+  patientRelationshipToPolicyHolder?: string | null,
+  memberId?: string | null,
+  policyGroupNumber?: string | null,
+};
 
-export interface PatientSetupMutation {
+export type PatientSetupMutation = {
   // Setup patient creates the patient in the db AND in athena
-  patientSetup: ShortPatientFragment;
-}
+  patientSetup:  {
+    id: string,
+    firstName: string | null,
+    middleName: string | null,
+    lastName: string | null,
+    language: string | null,
+    gender: string | null,
+    dateOfBirth: string | null,
+    zip: number | null,
+    createdAt: string | null,
+    consentToText: boolean | null,
+    consentToCall: boolean | null,
+  } | null,
+};
 
-export interface FullAppointmentFragment {
-  athenaAppointmentId: string;
-  dateTime: string;
-  athenaDepartmentId: number;
-  status: AppointmentStatus;
-  athenaPatientId: number;
-  duration: number;
-  appointmentTypeId: number;
-  appointmentType: string;
-  athenaProviderId: number;
-  userId: string;
-  patientId: string;
-  clinicId: string;
-}
+export type FullAppointmentFragment = {
+  athenaAppointmentId: string,
+  dateTime: string,
+  athenaDepartmentId: number,
+  status: AppointmentStatus,
+  athenaPatientId: number,
+  duration: number,
+  appointmentTypeId: number,
+  appointmentType: string,
+  athenaProviderId: number,
+  userId: string,
+  patientId: string,
+  clinicId: string,
+};
 
-export interface FullClinicFragment {
-  id: string;
-  name: string;
-}
+export type FullClinicFragment = {
+  id: string,
+  name: string,
+};
 
-export interface FullPatientEncounterFragment {
-  encounterType: string;
-  providerName: string;
-  providerRole: string;
-  location: string;
-  diagnoses: Array< {
+export type FullPatientEncounterFragment = {
+  encounterType: string,
+  providerName: string,
+  providerRole: string,
+  location: string,
+  diagnoses:  Array< {
     code: string,
     codeSystem: string,
     description: string,
-  } >;
-  reasons: Array< string | null >;
-  dateTime: string;
-}
+  } >,
+  reasons: Array< string | null >,
+  dateTime: string,
+};
 
-export interface FullPatientHealthRecordFragment {
-  id: string;
-  firstName: string | null;
-  lastName: string | null;
-  dateOfBirth: string | null;
-  gender: string | null;
-  suffix: string | null;
-  preferredName: string | null;
-  raceName: string | null;
-  race: string | null;
-  status: string | null;
-  ssn: string | null;
-  homebound: boolean | null;
-  language6392code: string | null;
-  maritalStatus: string | null;
-  maritalStatusName: string | null;
-  patientPhoto: boolean | null;
-  patientPhotoUrl: string | null;
-}
+export type FullPatientHealthRecordFragment = {
+  id: string,
+  firstName: string | null,
+  lastName: string | null,
+  dateOfBirth: string | null,
+  gender: string | null,
+  suffix: string | null,
+  preferredName: string | null,
+  raceName: string | null,
+  race: string | null,
+  status: string | null,
+  ssn: string | null,
+  homebound: boolean | null,
+  language6392code: string | null,
+  maritalStatus: string | null,
+  maritalStatusName: string | null,
+  patientPhoto: boolean | null,
+  patientPhotoUrl: string | null,
+};
 
-export interface FullPatientMedicationFragment {
-  medicationId: string;
-  name: string;
-  quantity: string | null;
-  quantityUnit: string | null;
-  dosageInstructions: string | null;
-  startDate: string | null;
-}
+export type FullPatientMedicationFragment = {
+  medicationId: string,
+  name: string,
+  quantity: string | null,
+  quantityUnit: string | null,
+  dosageInstructions: string | null,
+  startDate: string | null,
+};
 
-export interface FullPatientScratchPadFragment {
-  text: string | null;
-}
+export type FullPatientScratchPadFragment = {
+  text: string | null,
+};
 
-export interface FullUserFragment {
-  id: string;
-  locale: string | null;
-  firstName: string | null;
-  lastName: string | null;
-  userRole: UserRole;
-  email: string | null;
-  homeClinicId: string;
-  googleProfileImageUrl: string | null;
-}
+export type FullUserFragment = {
+  id: string,
+  locale: string | null,
+  firstName: string | null,
+  lastName: string | null,
+  userRole: UserRole,
+  email: string | null,
+  homeClinicId: string,
+  googleProfileImageUrl: string | null,
+};
 
-export interface ShortPatientFragment {
-  id: string;
-  firstName: string | null;
-  middleName: string | null;
-  lastName: string | null;
-  language: string | null;
-  gender: string | null;
-  dateOfBirth: string | null;
-  zip: number | null;
-  createdAt: string | null;
-  consentToText: boolean | null;
-  consentToCall: boolean | null;
-}
+export type ShortPatientFragment = {
+  id: string,
+  firstName: string | null,
+  middleName: string | null,
+  lastName: string | null,
+  language: string | null,
+  gender: string | null,
+  dateOfBirth: string | null,
+  zip: number | null,
+  createdAt: string | null,
+  consentToText: boolean | null,
+  consentToCall: boolean | null,
+};
 /* tslint:enable */

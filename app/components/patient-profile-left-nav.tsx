5cd8675856bf594702e2bc47afd8a526fe729b28
Logan Hasson <logan.hasson@gmail.com>
2017-07-13 09:21:02 -0400

    Ipad sidebar display (#213)
    * left nav component scaffold
    
    * moved everything over
    
    * Super basic styling done
    
    * Fix a silly thing
    
    * css fixes for desktop left rail
    
    * Missed a couple things
    
    * Change patient main info display for ipad
    
    * Snapshot
    
    * oh, look, another snapshot
    
    * i am a dope

---
+++ app/components/patient-profile-left-nav.tsx
@@ -0,0 +1,100 @@
+import * as classNames from 'classnames';
+import * as React from 'react';
+import { InjectedIntl } from 'react-intl';
+import CareTeamWidget from '../components/care-team-widget';
+import PatientLeftNavInfo from '../components/patient-left-nav-info';
+import PatientMedications from '../components/patient-medications';
+import * as styles from '../css/components/patient-profile-left-nav.css';
+import { ShortPatientFragment } from '../graphql/types';
+import { Size } from '../reducers/browser-reducer';
+
+export interface IProps {
+  intl: InjectedIntl;
+  browserSize: Size;
+  patient?: ShortPatientFragment;
+  patientId: string;
+}
+
+export interface IState {
+  selectedItem: string | null;
+}
+
+type SelectableItem = 'profile' | 'medications' | 'chat';
+
+export default class PatientProfileLeftNav extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+
+    this.state = { selectedItem: null };
+  }
+
+  componentWillReceiveProps(nextProps: IProps) {
+    this.setState(() => ({}));
+  }
+
+  onClick(clickedItem: SelectableItem) {
+    const { selectedItem } = this.state;
+
+    if (clickedItem === selectedItem) {
+      this.setState(() => ({ selectedItem: null }));
+    } else {
+      this.setState(() => ({ selectedItem: clickedItem }));
+    }
+  }
+
+  render() {
+    const { browserSize, patient, patientId, intl } = this.props;
+    const { selectedItem } = this.state;
+
+    const shortName = patient ? `${patient.firstName} ${patient.lastName}` : 'Loading Patient' ;
+
+    const profileButtonStyles = classNames(styles.smallButton, styles.profileIcon, {
+      [styles.selected]: selectedItem === 'profile',
+    });
+    const medsButtonStyles = classNames(styles.smallButton, styles.medsIcon, {
+      [styles.selected]: selectedItem === 'medications',
+    });
+    const chatButtonStyles = classNames(styles.smallChatButton, {
+      [styles.selected]: selectedItem === 'chat',
+    });
+
+    if (browserSize === 'small') {
+      return (
+        <div className={styles.smallLeftPane}>
+          <div className={styles.smallLeftPaneTop}>
+            <div className={styles.smallPatientRiskColor}></div>
+            <div
+              className={styles.smallPatientPhoto}
+              style={{ backgroundImage: `url('http://bit.ly/2u9bJDA')`}}>
+            </div>
+            <div className={styles.smallPatientName}>{shortName}</div>
+            <div
+              className={profileButtonStyles}
+              onClick={() => this.onClick('profile')}>
+            </div>
+            <div
+              className={medsButtonStyles}
+              onClick={() => this.onClick('medications')}>
+            </div>
+          </div>
+          <div className={styles.smallLeftPaneBottom}>
+            <div
+              className={chatButtonStyles}
+              onClick={() => this.onClick('chat')}>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.leftPane}>
+          <PatientLeftNavInfo intl={intl} patientId={patientId} patient={patient} />
+          <PatientMedications patientId={patientId} />
+          <CareTeamWidget patientId={patientId} />
+        </div>
+      );
+    }
+  }
+}
--- app/containers/patient-profile.tsx
+++ app/containers/patient-profile.tsx
@@ -1,23 +1,19 @@
 import * as classNames from 'classnames';
-import * as langs from 'langs';
-import * as moment from 'moment';
 import * as React from 'react';
 import { compose, graphql } from 'react-apollo';
 import { injectIntl, FormattedMessage, InjectedIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import CareTeamWidget from '../components/care-team-widget';
 import PatientEncounters from '../components/patient-encounters';
 import PatientInfo from '../components/patient-info';
-import PatientMedications from '../components/patient-medications';
-import PatientScratchPad from '../components/patient-scratch-pad';
+import PatientProfileLeftNav from '../components/patient-profile-left-nav';
 import PatientTasks from '../components/patient-tasks';
-import { DOB_FORMAT } from '../config';
 import * as styles from '../css/components/patient-profile-scene.css';
 import * as tabStyles from '../css/shared/tabs.css';
 import * as patientQuery from '../graphql/queries/get-patient.graphql';
 import { ShortPatientFragment } from '../graphql/types';
+import { Size } from '../reducers/browser-reducer';
 import { IState as IAppState } from '../store';
 
 type SelectableTabs = 'encounters' | 'patientInfo' | 'tasks';
@@ -30,6 +26,7 @@ export interface IProps {
   loading: boolean;
   error?: string;
   patient?: ShortPatientFragment;
+  browserSize: Size;
   match: {
     params: {
       patientId: string;
@@ -38,8 +35,7 @@ export interface IProps {
   };
 }
 
-const GENDER: any = { F: 'Female', M: 'Male' };
-const getPatientName = (patient: ShortPatientFragment) => (
+export const getPatientName = (patient: ShortPatientFragment) => (
   [patient.firstName, patient.middleName, patient.lastName].filter(Boolean).join(' ')
 );
 
@@ -52,21 +48,7 @@ class PatientProfileContainer extends React.Component<IProps, {}> {
   }
 
   render() {
-    const { patientId, patient, loading, error, intl, tabId, taskId } = this.props;
-
-    const name = patient ? getPatientName(patient) : null;
-    const patientAge = patient && patient.dateOfBirth ?
-      moment(patient.dateOfBirth, DOB_FORMAT).fromNow(true).replace('years', '') :
-      'Unknown';
-    const dateOfBirth = patient && patient.dateOfBirth ?
-      intl.formatDate(patient.dateOfBirth) :
-      'Unknown';
-    const patientJoined = patient && patient.createdAt ?
-      intl.formatRelative(patient.createdAt) :
-      'Unknown';
-    const gender = patient && patient.gender ? GENDER[patient.gender] : null;
-    const zip = patient && patient.zip ? patient.zip : null;
-    const language = patient && patient.language ? langs.where('1', patient.language).name : null;
+    const { patientId, patient, loading, error, intl, tabId, taskId, browserSize } = this.props;
 
     const encountersTabStyles = classNames(tabStyles.tab, {
       [tabStyles.selectedTab]: tabId === 'encounters',
@@ -78,72 +60,19 @@ class PatientProfileContainer extends React.Component<IProps, {}> {
       [tabStyles.selectedTab]: tabId === 'tasks',
     });
 
+    const mainBodyStyle = classNames({
+      [styles.mainBody]: browserSize === 'large',
+      [styles.mainBodySmall]: browserSize === 'small',
+    });
+
     return (
       <div className={styles.container}>
-        <div className={styles.leftPane}>
-          <div className={styles.patientRisk}>Medium Risk Patient</div>
-          <div className={styles.patientMain}>
-            <div className={styles.patientHeader}>
-              <div className={styles.patientPhoto}>
-                <img src={
-                  'https://lh4.googleusercontent.com/-fgvDmb1D_Aw/AAAAAAAAAAI/AAAAAAAAAAs' +
-                  '/MOKfIO7GrdY/s96-c/photo.jpg'}
-                />
-              </div>
-              <div className={styles.patientTitle}>
-                <div className={styles.patientName}>{name}</div>
-                <div className={styles.patientSubheading}>{patientAge} • {gender}</div>
-              </div>
-            </div>
-            <div className={styles.patientBasicInfo}>
-              <div className={styles.patientBasicInfoRow}>
-                <FormattedMessage id='patient.dateOfBirth'>
-                  {(message: string) =>
-                    <div className={styles.patientBasicInfoRowTitle}>{message}:</div>}
-                </FormattedMessage>
-                <div className={styles.patientBasicInfoRowData}>{dateOfBirth}</div>
-              </div>
-              <div className={styles.patientBasicInfoRow}>
-                <FormattedMessage id='patient.language'>
-                  {(message: string) =>
-                    <div className={styles.patientBasicInfoRowTitle}>{message}:</div>}
-                </FormattedMessage>
-                <div className={styles.patientBasicInfoRowData}>{language}</div>
-              </div>
-              <div className={styles.patientBasicInfoRow}>
-                <FormattedMessage id='patient.location'>
-                  {(message: string) =>
-                    <div className={styles.patientBasicInfoRowTitle}>{message}:</div>}
-                </FormattedMessage>
-                <div className={styles.patientBasicInfoRowData}>{zip}</div>
-              </div>
-              <div className={styles.patientBasicInfoRow}>
-                <FormattedMessage id='patient.joinedAt'>
-                  {(message: string) =>
-                    <div className={styles.patientBasicInfoRowTitle}>{message}:</div>}
-                </FormattedMessage>
-                <div className={styles.patientBasicInfoRowData}>{patientJoined}</div>
-              </div>
-              <div className={styles.patientBasicInfoRow}>
-                <FormattedMessage id='patient.medicareId'>
-                  {(message: string) =>
-                    <div className={styles.patientBasicInfoRowTitle}>{message}:</div>}
-                </FormattedMessage>
-                <div className={styles.patientBasicInfoRowData}>123456789</div>
-              </div>
-            </div>
-            <div className={styles.patientCommunication}>
-              <div className={classNames(styles.patientCommItem, styles.patientCommPhone)}></div>
-              <div className={classNames(styles.patientCommItem, styles.patientCommText)}></div>
-              <div className={classNames(styles.patientCommItem, styles.patientCommEmail)}></div>
-              <div className={classNames(styles.patientCommItem, styles.patientCommVideo)}></div>
-            </div>
-            <PatientScratchPad patientId={patientId} />
-          </div>
-          <PatientMedications patientId={patientId} />
-          <CareTeamWidget patientId={patientId} />
-        </div>
-        <div className={styles.mainBody}>
+        <PatientProfileLeftNav
+          browserSize={browserSize}
+          intl={intl}
+          patientId={patientId}
+          patient={patient} />
+        <div className={mainBodyStyle}>
           <div className={tabStyles.tabs}>
             <FormattedMessage id='patient.encounters'>
               {(message: string) =>
@@ -183,7 +112,9 @@ class PatientProfileContainer extends React.Component<IProps, {}> {
 }
 
 function mapStateToProps(state: IAppState, ownProps: IProps): Partial<IProps> {
+  const { browser } = state;
   return {
+    browserSize: browser ? browser.size : 'large',
     patientId: ownProps.match.params.patientId,
     tabId: ownProps.match.params.tabId || 'encounters',
   };

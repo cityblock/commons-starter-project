0016457bc90db9a35ce485bc844d00b531521de2
Brennan Moore <brennanmoore@gmail.com>
2017-09-07 14:12:20 -0400

    fix unhandleded promise warnings (#368)

--- app/patient-profile-container/__tests__/patient-care-plan-view.spec.tsx
+++ app/patient-profile-container/__tests__/patient-care-plan-view.spec.tsx
@@ -1,5 +1,6 @@
 import { createMemoryHistory } from 'history';
 import * as React from 'react';
+import { gql } from 'react-apollo';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { ConnectedRouter } from 'react-router-redux';
 import { create } from 'react-test-renderer';
@@ -11,10 +12,183 @@ import PatientCarePlanView from '../patient-care-plan-view';
 const locale = { messages: ENGLISH_TRANSLATION.messages };
 const mockStore = configureMockStore([]);
 
+const query = gql(`
+query getPatientCarePlan($patientId: String!) {
+  carePlanForPatient(patientId: $patientId) {
+    concerns {
+      ...FullPatientConcern
+      __typename
+    }
+    goals {
+      ...FullPatientGoal
+      __typename
+    }
+    __typename
+  }
+}
+
+fragment FullPatientConcern on PatientConcern {
+  id
+  order
+  concernId
+  concern {
+    ...FullConcern
+    __typename
+  }
+  patientId
+  patient {
+    ...ShortPatient
+    __typename
+  }
+  patientGoals {
+    ...FullPatientGoal
+    __typename
+  }
+  startedAt
+  completedAt
+  createdAt
+  updatedAt
+  deletedAt
+  __typename
+}
+
+fragment FullConcern on Concern {
+  id
+  title
+  createdAt
+  updatedAt
+  deletedAt
+  __typename
+}
+
+fragment ShortPatient on Patient {
+  id
+  firstName
+  middleName
+  lastName
+  language
+  gender
+  dateOfBirth
+  zip
+  createdAt
+  consentToText
+  consentToCall
+  __typename
+}
+
+fragment FullPatientGoal on PatientGoal {
+  id
+  title
+  patientId
+  patient {
+    ...ShortPatient
+    __typename
+  }
+  patientConcernId
+  goalSuggestionTemplateId
+  goalSuggestionTemplate {
+    ...FullGoalSuggestionTemplate
+    __typename
+  }
+  tasks {
+    ...FullTask
+    __typename
+  }
+  createdAt
+  updatedAt
+  deletedAt
+  __typename
+}
+
+fragment FullGoalSuggestionTemplate on GoalSuggestionTemplate {
+  id
+  title
+  taskTemplates {
+    ...FullTaskTemplate
+    __typename
+  }
+  createdAt
+  updatedAt
+  deletedAt
+  __typename
+}
+
+fragment FullTaskTemplate on TaskTemplate {
+  id
+  title
+  completedWithinNumber
+  completedWithinInterval
+  repeating
+  goalSuggestionTemplateId
+  priority
+  careTeamAssigneeRole
+  createdAt
+  updatedAt
+  deletedAt
+  __typename
+}
+
+fragment FullTask on Task {
+  id
+  title
+  description
+  createdAt
+  updatedAt
+  completedAt
+  deletedAt
+  dueAt
+  patientId
+  priority
+  patient {
+    id
+    firstName
+    middleName
+    lastName
+    __typename
+  }
+  assignedTo {
+    id
+    firstName
+    lastName
+    googleProfileImageUrl
+    userRole
+    __typename
+  }
+  followers {
+    id
+    firstName
+    lastName
+    googleProfileImageUrl
+    userRole
+    __typename
+  }
+  createdBy {
+    id
+    firstName
+    lastName
+    googleProfileImageUrl
+    userRole
+    __typename
+  }
+  __typename
+}`);
+
 it('renders patient care plan view', () => {
   const history = createMemoryHistory();
   const tree = create(
-    <MockedProvider mocks={[]} store={mockStore({ locale })}>
+    <MockedProvider mocks={[{
+      request: {
+        query,
+        variables: {
+          patientId: 'patient-1',
+        },
+      },
+      result: {
+        data: {
+          patientCarePlan: {},
+        },
+      },
+    }]} store={mockStore({ locale })}>
       <ReduxConnectedIntlProvider>
         <ConnectedRouter history={history}>
           <PatientCarePlanView
--- app/patient-profile-container/__tests__/patient-tasks.spec.tsx
+++ app/patient-profile-container/__tests__/patient-tasks.spec.tsx
@@ -1,5 +1,6 @@
 import { createMemoryHistory } from 'history';
 import * as React from 'react';
+import { gql } from 'react-apollo';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { ConnectedRouter } from 'react-router-redux';
 import { create } from 'react-test-renderer';
@@ -10,6 +11,73 @@ import PatientTasks from '../patient-tasks';
 
 const locale = { messages: ENGLISH_TRANSLATION.messages };
 const mockStore = configureMockStore([]);
+
+/* tslint:disable:max-line-length */
+const query = gql(`
+query getPatientTasks($patientId: String!, $pageNumber: Int, $pageSize: Int, $orderBy: TaskOrderOptions)
+{
+  tasksForPatient(patientId: $patientId, pageNumber: $pageNumber, pageSize: $pageSize, orderBy: $orderBy) {
+    edges {
+      node {
+        ...FullTask
+        __typename
+      }
+      __typename
+    }
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      __typename
+    }
+    __typename
+  }
+}
+
+fragment FullTask on Task {
+  id
+  title
+  description
+  createdAt
+  updatedAt
+  completedAt
+  deletedAt
+  dueAt
+  patientId
+  priority
+  patient {
+    id
+    firstName
+    middleName
+    lastName
+    __typename
+  }
+  assignedTo {
+    id
+    firstName
+    lastName
+    googleProfileImageUrl
+    userRole
+    __typename
+  }
+  followers {
+    id
+    firstName
+    lastName
+    googleProfileImageUrl
+    userRole
+    __typename
+  }
+  createdBy {
+    id
+    firstName
+    lastName
+    googleProfileImageUrl
+    userRole
+    __typename
+  }
+  __typename
+}`);
+/* tslint:enable:max-line-length */
 
 it('renders patient tasks', () => {
   const patient = {
@@ -44,9 +112,7 @@ it('renders patient tasks', () => {
     completedById: user.id,
   };
   const tasksResponse = {
-    edges: [
-      { node: task },
-    ],
+    edges: [{ node: task }],
     pageInfo: {
       hasNextPage: true,
       hasPreviousPage: true,
@@ -54,14 +120,42 @@ it('renders patient tasks', () => {
   };
   const history = createMemoryHistory();
   const tree = create(
-    <MockedProvider mocks={[]} store={mockStore({ locale, task })}>
+    <MockedProvider
+      mocks={[
+        {
+          request: {
+            query,
+            variables: {
+              patientId: patient.id,
+              pageNumber: 0,
+              pageSize: 10,
+              orderBy: 'createdAtDesc',
+            },
+          },
+          result: {
+            data: {
+              tasksForPatient: {
+                edges: [],
+                pageInfo: {
+                  hasNextPage: false,
+                  hasPreviousPage: false,
+                  __typename: 'PageInfo',
+                },
+              },
+            },
+          },
+        },
+      ]}
+      store={mockStore({ locale, task })}
+    >
       <ReduxConnectedIntlProvider>
         <ConnectedRouter history={history}>
           <PatientTasks
             updatePageParams={() => false}
             refetchTasks={() => false}
             tasksResponse={tasksResponse}
-            patientId={patient.id} />
+            patientId={patient.id}
+          />
         </ConnectedRouter>
       </ReduxConnectedIntlProvider>
     </MockedProvider>,

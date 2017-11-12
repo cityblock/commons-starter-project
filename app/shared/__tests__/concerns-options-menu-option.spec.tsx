dea3922796a2e43b5361a987fa2b4cd15a221ca9
Laura Cressman <lauracressman@mac.com>
2017-11-12 18:00:43 -0500

    Support CSS modules in tests (#564)
    * add Laura to seeds file
    
    * Install Jest library to mock CSS modules
    
    * Use Jest docs to handle CSS modueles in tests
    
    * Install identity-obj proxy and set up per docs
    
    * Update snapshot files accordingly, which now have mocked
    class names rather than undefined
    
    * Write example usage of test in concerns options menu option
    
    * See Jest docs here: https://facebook.github.io/jest/docs/en/webpack.html

--- app/shared/__tests__/concerns-options-menu-option.spec.tsx
+++ app/shared/__tests__/concerns-options-menu-option.spec.tsx
@@ -3,12 +3,17 @@ import * as React from 'react';
 import PatientConcernOption from '../concerns/options-menu/option';
 
 describe('Patient Concern Option Menu Item', () => {
-  it('renders label and icon', () => {
-    const label = 'Add a new goal';
-    const icon = 'add';
-    const wrapper = shallow(<PatientConcernOption label={label} icon={icon} />);
+  const label = 'Add a new goal';
+  const icon = 'add';
+  const wrapper = shallow(<PatientConcernOption label={label} icon={icon} />);
 
+  it('renders label and icon', () => {
     expect(wrapper.find('p').props().children).toBe(label);
     expect(wrapper.find('i').length).toBe(1);
+  });
+
+  it('applies correct styling to menu items', () => {
+    expect(wrapper.find('.option').length).toBe(1);
+    expect(wrapper.find('i').props().className).toBe('addIcon');
   });
 });

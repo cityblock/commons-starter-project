a70c8b72532e812bfde8968c9ac40ffd6d8f8d9b
Logan Hasson <logan.hasson@gmail.com>
2017-11-30 10:07:27 -0500

    stub date (#668)

--- app/shared/task/__tests__/task-due.spec.tsx
+++ app/shared/task/__tests__/task-due.spec.tsx
@@ -1,17 +1,27 @@
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import * as React from 'react';
 import DateInput from '../../library/date-input/date-input';
 import Icon from '../../library/icon/icon';
 import TaskDue from '../task-due';
 
-describe('Task Due Date Component', () => {
-  const dueAt = '2017-12-01 00:00:00-05';
-  const taskId = 'aryaStark';
-  const editTask = () => true as any;
+const oldDate = Date.now;
+const dueAt = '2017-12-01 00:00:00-05';
+const taskId = 'aryaStark';
+const editTask = () => true as any;
 
-  const wrapper = shallow(
-    <TaskDue completedAt="" dueAt={dueAt} taskId={taskId} editTask={editTask} />,
-  );
+describe('Task Due Date Component', () => {
+  let wrapper: ShallowWrapper<any, any>;
+
+  beforeAll(() => {
+    Date.now = jest.fn(() => 1500494779252);
+
+    wrapper = shallow(
+      <TaskDue completedAt="" dueAt={dueAt} taskId={taskId} editTask={editTask} />,
+    );
+  });
+  afterAll(() => {
+    Date.now = oldDate;
+  });
 
   it('renders date input element', () => {
     expect(wrapper.find(DateInput).length).toBe(1);

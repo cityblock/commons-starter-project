d241e1110ac98d38569cb26057ab12531dc13e49
Brennan Moore <brennanmoore@gmail.com>
2017-12-02 16:28:01 -0500

    Add logging middleware in development (#688)
    * add logging middleware in development
    
    * refactor a bit

--- app/client.tsx
+++ app/client.tsx
@@ -1,27 +1,18 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
-import { setContext } from 'apollo-link-context';
-import { HttpLink } from 'apollo-link-http';
 import createHistory from 'history/createBrowserHistory';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import { getMiddlewareLink } from './middleware-link';
 import ReduxConnectedIntlProvider from './redux-connected-intl-provider';
 import Routes from './routes';
 import createStore from './store';
 
-const httpLink = new HttpLink({ uri: '/graphql' });
-
-const middlewareLink = setContext(() => ({
-  headers: {
-    auth_token: localStorage.getItem('authToken'),
-  },
-}));
-
 const client = new ApolloClient<any>({
-  link: middlewareLink.concat(httpLink),
+  link: getMiddlewareLink(),
   cache: new InMemoryCache().restore({}) as any,
 });
 
---
+++ app/middleware-link.ts
@@ -0,0 +1,30 @@
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+import { HttpLink } from 'apollo-link-http';
+
+export const getMiddlewareLink = () => {
+  const httpLink = new HttpLink({ uri: '/graphql' });
+
+  let middlewareLink = setContext(() => ({
+    headers: {
+      auth_token: localStorage.getItem('authToken'),
+    },
+  }));
+
+  const loggingMiddleware = new ApolloLink(
+    (operation, forward) =>
+      forward
+        ? forward(operation).map(data => {
+            /* tslint:disable no-console */
+            console.log(operation);
+            /* tslint:enable no-console */
+            return data;
+          })
+        : null,
+  );
+
+  if (process.env.NODE_ENV === 'development') {
+    middlewareLink = middlewareLink.concat(loggingMiddleware);
+  }
+  return middlewareLink.concat(httpLink);
+};

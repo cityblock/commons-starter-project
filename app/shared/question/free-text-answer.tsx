043404a1c9693685833fed5e4e2713b79a692a0b
Brennan Moore <brennanmoore@gmail.com>
2017-12-11 12:47:26 -0500

    debounce free text answers correctly (#758)

--- app/shared/question/free-text-answer.tsx
+++ app/shared/question/free-text-answer.tsx
@@ -1,3 +1,4 @@
+import { debounce } from 'lodash';
 import * as React from 'react';
 import { FullQuestionFragment } from '../../graphql/types';
 import * as formStyles from '../css/forms.css';
@@ -5,7 +6,7 @@ import * as styles from './patient-question.css';
 
 interface IProps {
   editable: boolean;
-  currentAnswer: { id: string; value: string };
+  currentAnswer?: { id: string; value: string };
   question: FullQuestionFragment;
   onChange: (
     questionId: string,
@@ -13,24 +14,56 @@ interface IProps {
   ) => any;
 }
 
-export default class FreeTextAnswer extends React.Component<IProps, {}> {
+interface IState {
+  text: string;
+}
+
+const SAVE_TIMEOUT_MILLISECONDS = 500;
+
+export default class FreeTextAnswer extends React.Component<IProps, IState> {
+  save: (questionId: string, answers: Array<{ answerId: string; value: string | number }>) => any;
+
+  constructor(props: IProps) {
+    super(props);
+    this.save = debounce(props.onChange, SAVE_TIMEOUT_MILLISECONDS);
+    this.state = { text: props.currentAnswer ? props.currentAnswer.value : '' };
+  }
+
+  componentWillReceiveProps(nextProps: IProps) {
+    const { currentAnswer } = nextProps;
+
+    if (!this.props.currentAnswer && currentAnswer) {
+      this.setState({
+        text: currentAnswer.value,
+      });
+    }
+  }
+
+  onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { question } = this.props;
+    const answers = question.answers || [];
+    const answer = answers[0];
+    const answerId = answer.id;
+    this.setState({ text: event.target.value });
+    this.save(question.id, [{ answerId, value: event.target.value }]);
+  };
+
   render() {
-    const { question, currentAnswer, onChange, editable } = this.props;
+    const { question, editable } = this.props;
+
     const answers = question.answers || [];
 
-    // This is a weird answer type. Return nothing if there are no answers associated with it.
+    // Return nothing if there are no answers associated with it.
+    // Free text questions require a placeholder answer object to save the free text patient answer
     const answer = answers[0];
     if (answer) {
-      const answerId = answer.id;
-
       return (
         <div className={styles.questionBody}>
           <div className={styles.textArea}>
             <textarea
               disabled={!editable}
-              value={currentAnswer ? currentAnswer.value : ''}
-              // TODO: debounce onchange or do onunfocus
-              onChange={event => onChange(question.id, [{ answerId, value: event.target.value }])}
+              value={this.state.text}
+              onChange={this.onChange}
               className={formStyles.textarea}
             />
           </div>

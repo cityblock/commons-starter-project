6ac532980aa14b5170c54dc8e651ce9d91a4f060
Brennan Moore <brennanmoore@gmail.com>
2018-03-06 16:04:47 -0500

    dont double uglify (#1581)

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -3,16 +3,15 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = ({ production = false } = {}) => {
   const plugins = [
-    new webpack.WatchIgnorePlugin([/css\.d\.ts$/]),
     new webpack.EnvironmentPlugin(['NODE_ENV', 'GOOGLE_OAUTH_TOKEN']),
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
   ];
   if (!production) {
     plugins.push(
+      new webpack.WatchIgnorePlugin([/css\.d\.ts$/]),
       new webpack.HotModuleReplacementPlugin(),
       new ForkTsCheckerWebpackPlugin({
         tsconfig: path.resolve('tsconfig.webpack.json'),
@@ -40,16 +39,6 @@ module.exports = ({ production = false } = {}) => {
           to: '../server-compiled/server/handlers/pdf/fonts',
         },
       ]),
-      new UglifyJsPlugin({
-        exclude: /node_modules/,
-        sourceMap: true,
-        parallel: true,
-        uglifyOptions: {
-          ecma: 8,
-          warnings: true,
-          mangle: true,
-        },
-      }),
     );
   }
   return plugins;

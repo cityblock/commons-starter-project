4f5c3b3df55f34e6a320d789dbe8f8d2ae19d4d1
Brennan Moore <brennanmoore@gmail.com>
2018-03-21 15:29:16 -0400

    Make copy from production -> staging non-distructive (#1810)
    * make copy from production -> staging non-distructive
    
    * update risk area rows

--- scripts/aptible-copy-production-to-staging.sql
+++ scripts/aptible-copy-production-to-staging.sql
@@ -1,32 +1,32 @@
 begin;
-truncate table task_template, patient_task_suggestion, question_condition, task_template, concern_suggestion, concern, patient_list, answer, question, goal_suggestion_template, goal_suggestion, computed_field,  progress_note_template, risk_area, risk_area_group, screening_tool, screening_tool_score_range, task_suggestion, care_plan_suggestion, patient_concern, patient_answer, patient_goal, progress_note, risk_area_assessment_submission, patient_screening_tool_submission, care_plan_update_event, computed_field_flag, patient_answer_event, task, quick_call, task_event, task_comment, task_follower, event_notification, diagnosis_code, concern_diagnosis_code, cbo_referral, cbo, cbo_category;
 
-insert into risk_area_group (select * from production.risk_area_group);
-insert into risk_area (select * from production.risk_area);
+insert into risk_area_group (select * from production.risk_area_group) on conflict ON CONSTRAINT risk_area_group_pkey do update SET title = EXCLUDED.title, "shortTitle" = EXCLUDED."shortTitle", "order" = EXCLUDED."order", "deletedAt" = EXCLUDED."deletedAt", "mediumRiskThreshold" = EXCLUDED."mediumRiskThreshold", "highRiskThreshold" = EXCLUDED."highRiskThreshold";
 
-insert into screening_tool (select * from production.screening_tool);
-insert into screening_tool_score_range (select * from production.screening_tool_score_range);
+insert into risk_area (select * from production.risk_area) on conflict ON CONSTRAINT risk_area_pkey do update SET title = EXCLUDED.title, "order" = EXCLUDED."order", "deletedAt" = EXCLUDED."deletedAt", "mediumRiskThreshold" = EXCLUDED."mediumRiskThreshold", "highRiskThreshold" = EXCLUDED."highRiskThreshold";
 
-insert into progress_note_template (select * from production.progress_note_template);
+insert into screening_tool (select * from production.screening_tool) on conflict do nothing;
+insert into screening_tool_score_range (select * from production.screening_tool_score_range) on conflict do nothing;
 
-insert into computed_field (select * from production.computed_field);
-insert into goal_suggestion_template (select * from production.goal_suggestion_template);
+insert into progress_note_template (select * from production.progress_note_template) on conflict ON CONSTRAINT progress_note_template_pkey do update SET title = EXCLUDED.title, "deletedAt" = EXCLUDED."deletedAt";
 
-insert into cbo_category (select * from production.cbo_category);
-insert into cbo (select * from production.cbo);
+insert into computed_field (select * from production.computed_field) on conflict do nothing;
+insert into goal_suggestion_template (select * from production.goal_suggestion_template) on conflict do nothing;
 
-insert into question (select * from production.question);
-insert into answer (select * from production.answer);
-insert into patient_list (select * from production.patient_list);
-	
-insert into concern (select * from production.concern);
-insert into concern_suggestion (select * from production.concern_suggestion);
-insert into goal_suggestion (select * from production.goal_suggestion);
+insert into cbo_category (select * from production.cbo_category) on conflict do nothing;
+insert into cbo (select * from production.cbo) on conflict do nothing;
 
-insert into diagnosis_code (select * from production.diagnosis_code);
-insert into concern_diagnosis_code (select * from production.concern_diagnosis_code);
+insert into question (select * from production.question) on conflict do nothing;
+insert into answer (select * from production.answer) on conflict do nothing;
+insert into patient_list (select * from production.patient_list) on conflict do nothing;
 
-insert into task_template (select * from production.task_template);
-insert into question_condition (select * from production.question_condition);
+insert into concern (select * from production.concern) on conflict do nothing;
+insert into concern_suggestion (select * from production.concern_suggestion) on conflict do nothing;
+insert into goal_suggestion (select * from production.goal_suggestion) on conflict do nothing;
+
+insert into diagnosis_code (select * from production.diagnosis_code) on conflict do nothing;
+insert into concern_diagnosis_code (select * from production.concern_diagnosis_code) on conflict do nothing;
+
+insert into task_template (select * from production.task_template) on conflict do nothing;
+insert into question_condition (select * from production.question_condition) on conflict do nothing;
 
 commit;

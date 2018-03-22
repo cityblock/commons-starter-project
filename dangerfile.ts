865c3fe225aab6f28b8f6eeca0aa4c290d5586c7
Brennan Moore <brennanmoore@gmail.com>
2018-03-22 16:59:50 -0400

    Fix danger to handle new vs edited migration files (#1822)
    * test
    
    * hopefully fix danger
    
    * remove migration edit

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,11 +4,18 @@ import { includes } from 'lodash';
 
 const filesOnly = (file: string) => fs.existsSync(file) && fs.lstatSync(file).isFile();
 
-// Custom subsets of known files
 const modifiedMigrations = danger.git.modified_files
   .filter(p => includes(p, 'migrations/'))
   .filter(filesOnly);
 
 if (modifiedMigrations.length > 0) {
+  warn("This PR edited an existing migrations. Don't do that! cc: @zamiang");
+}
+
+const newMigrations = danger.git.created_files
+  .filter(p => includes(p, 'migrations/'))
+  .filter(filesOnly);
+
+if (newMigrations.length > 0) {
   warn('This PR added migrations cc: @tzfirac');
 }

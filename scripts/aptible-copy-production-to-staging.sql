a4fa64b4570d6189e28a8117e356aa721f19f3c5
Brennan Moore <brennanmoore@gmail.com>
2018-03-30 14:45:23 -0400

    improvements to prod -> staging copy (#1928)
    * improvements to prod -> staging copy
    
    * omg more tables

--- scripts/aptible-copy-production-to-staging.sql
+++ scripts/aptible-copy-production-to-staging.sql
@@ -1,32 +1,211 @@
-begin;
+BEGIN;
 
-insert into risk_area_group (select * from production.risk_area_group) on conflict ON CONSTRAINT risk_area_group_pkey do update SET title = EXCLUDED.title, "shortTitle" = EXCLUDED."shortTitle", "order" = EXCLUDED."order", "deletedAt" = EXCLUDED."deletedAt", "mediumRiskThreshold" = EXCLUDED."mediumRiskThreshold", "highRiskThreshold" = EXCLUDED."highRiskThreshold";
+/*
+* RISK AREA MODELS
+*/
+INSERT INTO risk_area_group
+SELECT
+  *
+FROM
+  production.risk_area_group ON CONFLICT ON CONSTRAINT risk_area_group_pkey DO
+  UPDATE
+    SET
+      title = EXCLUDED.title, "shortTitle" = EXCLUDED. "shortTitle", "order" = EXCLUDED. "order", "deletedAt" = EXCLUDED. "deletedAt", "mediumRiskThreshold" = EXCLUDED. "mediumRiskThreshold", "highRiskThreshold" = EXCLUDED. "highRiskThreshold";
 
-insert into risk_area (select * from production.risk_area) on conflict ON CONSTRAINT risk_area_pkey do update SET title = EXCLUDED.title, "order" = EXCLUDED."order", "deletedAt" = EXCLUDED."deletedAt", "mediumRiskThreshold" = EXCLUDED."mediumRiskThreshold", "highRiskThreshold" = EXCLUDED."highRiskThreshold";
+INSERT INTO risk_area
+SELECT
+  *
+FROM
+  production.risk_area ON CONFLICT ON CONSTRAINT risk_area_pkey DO
+  UPDATE
+    SET
+      title = EXCLUDED.title, "order" = EXCLUDED. "order", "deletedAt" = EXCLUDED. "deletedAt", "mediumRiskThreshold" = EXCLUDED. "mediumRiskThreshold", "highRiskThreshold" = EXCLUDED. "highRiskThreshold";
 
-insert into screening_tool (select * from production.screening_tool) on conflict do nothing;
-insert into screening_tool_score_range (select * from production.screening_tool_score_range) on conflict do nothing;
+/*
+* SCREENING TOOL MODELS
+*/
+INSERT INTO screening_tool
+SELECT
+  *
+FROM
+  production.screening_tool ON CONFLICT ON CONSTRAINT screening_tool_pkey DO
+  UPDATE
+    SET
+      title = EXCLUDED.title, "riskAreaId" = EXCLUDED. "riskAreaId", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into progress_note_template (select * from production.progress_note_template) on conflict ON CONSTRAINT progress_note_template_pkey do update SET title = EXCLUDED.title, "deletedAt" = EXCLUDED."deletedAt";
+INSERT INTO screening_tool_score_range
+SELECT
+  *
+FROM
+  production.screening_tool_score_range ON CONFLICT ON CONSTRAINT screening_tool_score_range_pkey DO
+  UPDATE
+    SET
+      "screeningToolId" = EXCLUDED. "screeningToolId", "description" = EXCLUDED. "description", "range" = EXCLUDED. "range", "riskAdjustmentType" = EXCLUDED. "riskAdjustmentType", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into computed_field (select * from production.computed_field) on conflict do nothing;
-insert into goal_suggestion_template (select * from production.goal_suggestion_template) on conflict do nothing;
+/*
+* PROGRESS NOTE MODELS
+*/
+INSERT INTO progress_note_template
+SELECT
+  *
+FROM
+  production.progress_note_template ON CONFLICT ON CONSTRAINT progress_note_template_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "requiresGlassBreak" = EXCLUDED. "requiresGlassBreak", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into cbo_category (select * from production.cbo_category) on conflict do nothing;
-insert into cbo (select * from production.cbo) on conflict do nothing;
+/*
+* COMPUTED FIELD MODEL
+*/
+INSERT INTO computed_field
+SELECT
+  *
+FROM
+  production.computed_field ON CONFLICT ON CONSTRAINT computed_field_pkey DO
+  UPDATE
+    SET
+      "slug" = EXCLUDED. "slug", "label" = EXCLUDED. "label", "dataType" = excluded. "dataType", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into question (select * from production.question) on conflict do nothing;
-insert into answer (select * from production.answer) on conflict do nothing;
-insert into patient_list (select * from production.patient_list) on conflict do nothing;
+/*
+*GOAL SUGGESTION TEMPLATE
+*/
+INSERT INTO goal_suggestion_template
+SELECT
+  *
+FROM
+  production.goal_suggestion_template ON CONFLICT ON CONSTRAINT goal_suggestion_template_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into concern (select * from production.concern) on conflict do nothing;
-insert into concern_suggestion (select * from production.concern_suggestion) on conflict do nothing;
-insert into goal_suggestion (select * from production.goal_suggestion) on conflict do nothing;
+/*
+* CBO MODELS
+*/
+INSERT INTO cbo_category
+SELECT
+  *
+FROM
+  production.cbo_category ON CONFLICT ON CONSTRAINT cbo_category_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into diagnosis_code (select * from production.diagnosis_code) on conflict do nothing;
-insert into concern_diagnosis_code (select * from production.concern_diagnosis_code) on conflict do nothing;
+INSERT INTO cbo
+SELECT
+  *
+FROM
+  production.cbo ON CONFLICT ON CONSTRAINT cbo_pkey DO
+  UPDATE
+    SET
+      "name" = EXCLUDED. "name", "categoryId" = EXCLUDED. "categoryId", "address" = EXCLUDED. "address", "city" = EXCLUDED. "city", "state" = EXCLUDED. "state", "zip" = EXCLUDED. "zip", "fax" = EXCLUDED. "fax", "phone" = EXCLUDED. "phone", "url" = EXCLUDED. "url", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-insert into task_template (select * from production.task_template) on conflict do nothing;
-insert into question_condition (select * from production.question_condition) on conflict do nothing;
+/*
+* QUESTION MODEL
+*/
+INSERT INTO question
+SELECT
+  *
+FROM
+  production.question ON CONFLICT ON CONSTRAINT question_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "validatedSource" = EXCLUDED. "validatedSource", "answerType" = EXCLUDED. "answerType", "applicableIfType" = EXCLUDED. "applicableIfType", "riskAreaId" = EXCLUDED. "riskAreaId", "order" = EXCLUDED. "order", "screeningToolId" = EXCLUDED. "screeningToolId", "progressNoteTemplateId" = EXCLUDED. "progressNoteTemplateId", "computedFieldId" = EXCLUDED. "computedFieldId", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
 
-commit;
+/*
+* ANSWER MODEL
+*/
+INSERT INTO answer
+SELECT
+  *
+FROM
+  production.answer ON CONFLICT ON CONSTRAINT answer_pkey DO
+  UPDATE
+    SET
+      "displayValue" = EXCLUDED. "displayValue", "value" = EXCLUDED. "value", "valueType" = EXCLUDED. "valueType", "riskAdjustmentType" = EXCLUDED. "riskAdjustmentType", "inSummary" = EXCLUDED. "inSummary", "summaryText" = EXCLUDED. "summaryText", "questionId" = EXCLUDED. "questionId", "order" = EXCLUDED. "order", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+INSERT INTO patient_list
+SELECT
+  *
+FROM
+  production.patient_list ON CONFLICT ON CONSTRAINT patient_list_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "order" = EXCLUDED. "order", "answerId" = EXCLUDED. "answerId", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+/*
+* CONCERN AND GOAL SUGGESTION MODEL
+*/
+INSERT INTO concern
+SELECT
+  *
+FROM
+  production.concern ON CONFLICT ON CONSTRAINT concern_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+INSERT INTO concern_suggestion
+SELECT
+  *
+FROM
+  production.concern_suggestion ON CONFLICT ON CONSTRAINT concern_suggestion_pkey DO
+  UPDATE
+    SET
+      "concernId" = EXCLUDED. "concernId", "answerId" = EXCLUDED. "answerId", "screeningToolScoreRangeId" = EXCLUDED. "screeningToolScoreRangeId", "deletedAt" = EXCLUDED. "deletedAt";
+
+INSERT INTO goal_suggestion
+SELECT
+  *
+FROM
+  production.goal_suggestion ON CONFLICT ON CONSTRAINT goal_suggestion_pkey DO
+  UPDATE
+    SET
+      "goalSuggestionTemplateId" = EXCLUDED. "goalSuggestionTemplateId", "answerId" = EXCLUDED. "answerId", "screeningToolScoreRangeId" = EXCLUDED. "screeningToolScoreRangeId", "deletedAt" = EXCLUDED. "deletedAt";
+
+/*
+* DIAGNOSIS CODE MODELS
+*/
+INSERT INTO diagnosis_code
+SELECT
+  *
+FROM
+  production.diagnosis_code ON CONFLICT ON CONSTRAINT diagnosis_code_pkey DO
+  UPDATE
+    SET
+      "codesetName" = EXCLUDED. "codesetName", "label" = EXCLUDED. "label", "version" = EXCLUDED. "version", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+INSERT INTO concern_diagnosis_code
+SELECT
+  *
+FROM
+  production.concern_diagnosis_code ON CONFLICT ON CONSTRAINT concern_diagnosis_code_pkey DO
+  UPDATE
+    SET
+      "diagnosisCodeId" = EXCLUDED. "diagnosisCodeId", "concernId" = EXCLUDED. "concernId", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+/*
+* TASK TEMPLATE MODEL
+*/
+INSERT INTO task_template
+SELECT
+  *
+FROM
+  production.task_template ON CONFLICT ON CONSTRAINT task_template_pkey DO
+  UPDATE
+    SET
+      "title" = EXCLUDED. "title", "goalSuggestionTemplateId" = EXCLUDED. "goalSuggestionTemplateId", "priority" = EXCLUDED. "priority", "repeating" = EXCLUDED. "repeating", "completedWithinNumber" = EXCLUDED. "completedWithinNumber", "completedWithinInterval" = EXCLUDED. "completedWithinInterval", "careTeamAssigneeRole" = EXCLUDED. "careTeamAssigneeRole", "CBOCategoryId" = EXCLUDED. "CBOCategoryId", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+/*
+* QUESTION CONDITION MODEL
+*/
+INSERT INTO question_condition
+SELECT
+  *
+FROM
+  production.question_condition ON CONFLICT ON CONSTRAINT question_condition_pkey DO
+  UPDATE
+    SET
+      "questionId" = EXCLUDED. "questionId", "answerId" = EXCLUDED. "answerId", "deletedAt" = EXCLUDED. "deletedAt", "updatedAt" = EXCLUDED. "updatedAt";
+
+COMMIT;
+

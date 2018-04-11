189fad172a971e86489420b4b105a153ab99db21
Laura Cressman <lauracressman@mac.com>
2018-04-11 12:04:34 -0400

    Remove duplicate phone numbers before adding unique index (#2001)
    * Fix migration
    
    * Run prettier
    
    * Fix migration
    
    * Run prettier

--- server/models/migrations/20180410150907_phone-deletedat-unique.js
+++ server/models/migrations/20180410150907_phone-deletedat-unique.js
@@ -1,13 +1,21 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.table('phone', table => {
-    // delete columns that we will move to join tables
-    table.dropColumn('type');
-    table.dropColumn('description');
-    table.dropColumn('updatedById');
-  }).raw(`
+exports.up = async function(knex, Promise) {
+  return knex.schema
+    .raw(
+      `
+    CREATE TEMPORARY SEQUENCE phonenum START 17185550000;
+    UPDATE phone
+      SET "phoneNumber"='+' || nextVal('phonenum')::text;
+
     CREATE UNIQUE INDEX phone_number_unique ON phone ("phoneNumber")
     WHERE "deletedAt" IS NULL;
-  `);
+  `,
+    )
+    .table('phone', table => {
+      // delete columns that we will move to join tables
+      table.dropColumn('type');
+      table.dropColumn('description');
+      table.dropColumn('updatedById');
+    });
 };
 
 exports.down = function(knex, Promise) {

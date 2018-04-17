4c8816777aea65405056435084ec00473aab707b
Brennan Moore <brennanmoore@gmail.com>
2018-04-16 21:45:37 -0400

    Minor test config improvements (#2100)
    * general test config cleanup
    
    * wip

--- enzyme-setup.js
+++ enzyme-setup.js
@@ -1,6 +1,6 @@
 var Enzyme = require('enzyme');
 var Adapter = require('enzyme-adapter-react-16');
 // Needed to run tests related to react-PDF generation
-import 'regenerator-runtime/runtime';
+require('regenerator-runtime/runtime');
 
 Enzyme.configure({ adapter: new Adapter() });

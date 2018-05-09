89418d53566de7f4bcfd9eebfe05f17396d4352d
Brennan Moore <brennanmoore@gmail.com>
2018-05-09 10:35:12 -0400

    Add dangerfile for node module updates (#2427)
    * add dangerfile for node module updates
    
    * add chart.js
    
    * improve message

--- dangerfile.ts
+++ dangerfile.ts
@@ -8,6 +8,29 @@ const modifiedMigrations = danger.git.modified_files
   .filter(p => includes(p, 'migrations/'))
   .filter(filesOnly);
 
+const checkPackageJson = async () => {
+  const changelogDiff = await danger.git.diffForFile('package.json');
+
+  if (changelogDiff) {
+    const problematicModules = [
+      'kue',
+      'react-beautiful-dnd',
+      '@react-pdf/core',
+      '@react-pdf/node',
+      'chart.js',
+    ];
+    problematicModules.map(name => {
+      if (changelogDiff.diff.includes(name)) {
+        warn(
+          `Please be careful updating the node module "${name}". Ensure you have tested locally and then test it on staging before deploy`,
+        );
+      }
+    });
+  }
+};
+
+checkPackageJson();
+
 if (modifiedMigrations.length > 0) {
   warn("This PR edited an existing migration. Don't do that! cc: @zamiang");
 }
@@ -17,7 +40,7 @@ const newMigrations = danger.git.created_files
   .filter(filesOnly);
 
 if (newMigrations.length > 0) {
-  warn('This PR added migrations cc: @tzfirac');
+  warn('This PR added migrations cc: @tzfirac @paradhaks');
   warn(
     'Did this PR add migrations for tables in builder? If so, be sure to update the builder foreign tables in the production db and then update the copy from staging to production script. More info [here](https://github.com/cityblock/commons#builder-in-production)',
   );

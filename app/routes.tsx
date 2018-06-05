b4f794211e3f041d431526973e0cf81a54fe709a
Brennan Moore <brennanmoore@gmail.com>
2018-06-05 14:21:58 -0400

    Use loadable for infrequent/admin parts of the app (#2702)
    * use loadable for infrequent/admin parts of the app
    
    * async load login

--- app/routes.tsx
+++ app/routes.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react';
+import * as Loadable from 'react-loadable';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Authentication from './authentication-container/authentication-container';
-import Builder from './builder-container/builder-container';
 import Calendar from './calendar-container/calendar-container';
-import Contacts from './contacts-container/contacts-container';
 import Dashboard from './dashboard-container/dashboard-container';
-import LogIn from './login-container/login-container';
 import Main from './main-container/main-container';
-import Manager from './manager-container/manager-container';
 import PatientPanel from './patient-panel-container/patient-panel-container';
 import PatientProfile from './patient-profile-container/patient-profile-container';
 import PatientSearch from './patient-search-container/patient-search-container';
-import Settings from './settings-container/settings-container';
 import withTracker from './shared/with-tracker/with-tracker';
 import Tasks from './tasks-container/tasks-container';
-import Voicemail from './voicemail-container/voicemail-container';
 
 const PatientRedirect = (options: any) => (
   <Redirect to={`/patients/${options.match.params.patientId}/map/active`} />
@@ -24,24 +19,69 @@ const PatientRedirect = (options: any) => (
  * What is <Switch>?
  * <Switch> makes the router behave like a normal router and render the first matched route
  * instead of all matched routes.
+ *
+ * NOTE: We async load infrequently used and admin routes
  */
 export default (
   <Main>
     <Switch>
-      <Route exact path="/" component={withTracker(LogIn)} />
+      <Route
+        exact
+        path="/"
+        component={(Loadable as any)({
+          loader: async () =>
+            import(/* webpackChunkName: "login" */ './login-container/login-container'),
+          loading: () => null,
+        })}
+      />
       <Authentication>
         <Route path="/dashboard/:list/:answerId?" component={withTracker(Dashboard)} />
         <Route path="/tasks/:tab/:taskId?" component={withTracker(Tasks)} />
         <Route path="/calendar" component={withTracker(Calendar)} />
-        <Route path="/settings" component={withTracker(Settings)} />
         <Route path="/search" component={withTracker(PatientSearch)} />
         <Route exact path="/patients/:patientId" component={PatientRedirect} />
         <Route exact path="/patients" component={withTracker(PatientPanel)} />
         <Route path="/patients/:patientId/:tab" component={withTracker(PatientProfile)} />
-        <Route path="/builder/:tab?/:objectId?/:subTab?/:subTabId?" component={Builder} />
-        <Route path="/manager/:tabId?/:objectId?" component={Manager} />
-        <Route path="/contacts" component={withTracker(Contacts)} />
-        <Route path="/voicemails/:voicemailId" component={withTracker(Voicemail)} />
+        <Route
+          path="/settings"
+          component={(Loadable as any)({
+            loader: async () =>
+              import(/* webpackChunkName: "settings" */ './settings-container/settings-container'),
+            loading: () => null,
+          })}
+        />
+        <Route
+          path="/builder/:tab?/:objectId?/:subTab?/:subTabId?"
+          component={(Loadable as any)({
+            loader: async () =>
+              import(/* webpackChunkName: "builder" */ './builder-container/builder-container'),
+            loading: () => null,
+          })}
+        />
+        <Route
+          path="/manager/:tabId?/:objectId?"
+          component={(Loadable as any)({
+            loader: async () =>
+              import(/* webpackChunkName: "manager" */ './manager-container/manager-container'),
+            loading: () => null,
+          })}
+        />
+        <Route
+          path="/contacts"
+          component={(Loadable as any)({
+            loader: async () =>
+              import(/* webpackChunkName: "contacts" */ './contacts-container/contacts-container'),
+            loading: () => null,
+          })}
+        />
+        <Route
+          path="/voicemails/:voicemailId"
+          component={(Loadable as any)({
+            loader: async () =>
+              import(/* webpackChunkName: "voicemail" */ './voicemail-container/voicemail-container'),
+            loading: () => null,
+          })}
+        />
       </Authentication>
     </Switch>
   </Main>

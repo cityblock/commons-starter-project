3c7d24d6f88faad35160f35ad8181f05b8b49d12
flaurida <lauracressman@mac.com>
2018-07-06 11:43:45 -0400

    Add test setup

--- server/models/seeds/dev/initial-seed.js
+++ server/models/seeds/dev/initial-seed.js
@@ -19,126 +19,21 @@ function deleteTables(knex, Promise) {
   });
 }
 
-function buildClinic(name, departmentId) {
+function buildPuppy(name) {
   return {
     id: uuid.v4(),
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
     name,
-    departmentId,
   };
 }
 
-function buildPhysician(firstName, lastName, email, homeClinicId, athenaProviderId) {
-  return {
-    id: uuid.v4(),
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    firstName,
-    lastName,
-    userRole: 'physician',
-    email,
-    homeClinicId,
-    athenaProviderId,
-  };
-}
-
-function buildPatient(firstName, lastName, homeClinicId, gender) {
-  return {
-    id: uuid.v4(),
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    firstName,
-    lastName,
-    homeClinicId,
-    gender,
-  };
-}
-
-function buildCareTeam(userId, patientId) {
-  return {
-    id: uuid.v4(),
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    userId,
-    patientId,
-  };
-}
-
-function createPhysicians(knex, clinicIds) {
+function createPuppies(knex, clinicIds) {
   return knex
-    .table('user')
-    .insert([
-      buildPhysician('Logan', 'Hasson', 'logan@cityblock.com', clinicIds[0], 2),
-      buildPhysician('Brennan', 'Moore', 'brennan@cityblock.com', clinicIds[0], 3),
-      buildPhysician('Frank', 'Guzzone', 'frank@cityblock.com', clinicIds[0], 4),
-      buildPhysician('Mat', 'Balez', 'mat@cityblock.com', clinicIds[0], 5),
-      buildPhysician('Peter', 'Weinberg', 'peter@cityblock.com', clinicIds[0], 7),
-      buildPhysician('Toyin', 'Ajayi', 'toyin@cityblock.com', clinicIds[0], 8),
-      buildPhysician('Iyah', 'Romm', 'iyah@cityblock.com', clinicIds[0], 9),
-      buildPhysician('Bay', 'Gross', 'bay@cityblock.com', clinicIds[0], 10),
-      buildPhysician('Ari', 'Rosner', 'ari@cityblock.com', clinicIds[0], 11),
-      buildPhysician('Ariane', 'Tschumi', 'ariane@cityblock.com', clinicIds[0], 12),
-      buildPhysician('Tristan', 'Williams', 'tristan@cityblock.com', clinicIds[0], 13),
-      buildPhysician('Laura', 'Cressman', 'laura@cityblock.com', clinicIds[0], 27),
-    ]);
-}
-
-function createPatients(knex, clinicIds) {
-  return knex
-    .table('patient')
-    .returning('id')
-    .insert([
-      buildPatient('Carmen', 'Vasbinder', clinicIds[0], 'female'),
-      buildPatient('Minh', 'Marceau', clinicIds[0], 'male'),
-      buildPatient('Florentino', 'Berge', clinicIds[0], 'male'),
-      buildPatient('Jacquiline', 'Santore', clinicIds[0], 'female'),
-      buildPatient('Lan', 'Overstreet', clinicIds[0], 'female'),
-      buildPatient('Toby', 'Hartshorn', clinicIds[0], 'male'),
-      buildPatient('Birdie', 'Wansley', clinicIds[0], 'female'),
-      buildPatient('Emery', 'Official', clinicIds[0], 'male'),
-      buildPatient('Candace', 'Kimberlin', clinicIds[0], 'female'),
-      buildPatient('Ula', 'Hertel', clinicIds[0], 'female'),
-      buildPatient('Rolland', 'Auman', clinicIds[0], 'male'),
-      buildPatient('Barney', 'Ober', clinicIds[0], 'male'),
-    ]);
-}
-
-function createCareTeam(knex, Promise, patientIds) {
-  return knex
-    .table('user')
-    .pluck('id')
-    .then(function(userIds) {
-      const careTeamPromises = [];
-      for (let i = 0; i < patientIds.length; i++) {
-        for (let j = 0; j < userIds.length; j++) {
-          careTeamPromises.push(
-            knex.table('care_team').insert(buildCareTeam(userIds[j], patientIds[i])),
-          );
-        }
-      }
-      return Promise.all(careTeamPromises);
-    });
+    .table('puppy')
+    .insert([buildPuppy('Katy'), buildPuppy('Rose'), buildPuppy('Lord Barkington')]);
 }
 
 const seed = function(knex, Promise) {
-  return knex
-    .table('clinic')
-    .returning('id')
-    .insert(buildClinic('Clinic Zero', 1))
-    .then(function(clinicIds) {
-      return createPhysicians(knex, clinicIds).then(function() {
-        return knex
-          .table('clinic')
-          .pluck('id')
-          .then(function(clinicIds) {
-            return createPatients(knex, clinicIds);
-          })
-          .then(function(patientIds) {
-            return createCareTeam(knex, Promise, patientIds);
-          });
-      });
-    });
+  return createPuppies(knex);
 };
 
 exports.seed = function(knex, Promise) {

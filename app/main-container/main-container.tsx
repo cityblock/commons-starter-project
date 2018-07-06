242db4308b22cdf6231a488ce9e4ebe5f99cdd5f
flaurida <lauracressman@mac.com>
2018-07-06 13:51:19 -0400

    Rip out example part 2

--- app/main-container/main-container.tsx
+++ app/main-container/main-container.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import puppiesGraphql from '../graphql/queries/get-puppies.graphql';
-import { getPuppies } from '../graphql/types';
 import styles from './css/main.css';
-import { ApolloError } from 'apollo-client';
 
-interface IGraphqlProps {
-  puppies: getPuppies['puppies'];
-  loading: boolean;
-  error?: ApolloError | null;
-}
-
-export const MainContainer: React.StatelessComponent<IGraphqlProps> = (props: IGraphqlProps) => {
-  const { puppies, loading, error } = props;
-
-  if (loading || error) {
-    return <h1>Loading...</h1>;
-  }
-
-  const renderedPuppies = puppies.map(puppy => {
-    return <p key={puppy.id}>{puppy.name}</p>;
-  });
-
-  return <div className={styles.body}>{renderedPuppies}</div>;
+export const MainContainer: React.StatelessComponent = () => {
+  return (
+    <div className={styles.body}>
+      <h1>Your Project here!</h1>
+    </div>
+  );
 };
 
-export default graphql(puppiesGraphql, {
-  props: ({ data }): IGraphqlProps => ({
-    loading: data ? data.loading : false,
-    error: data ? data.error : null,
-    puppies: data ? (data as any).puppies : null,
-  }),
-})(MainContainer);
+export default MainContainer;

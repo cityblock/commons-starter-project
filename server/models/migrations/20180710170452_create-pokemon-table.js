3d861f38482294b0f77f5c0b80142bd68d001b55
Alicia Underhill <alicia.underhill@gmail.com>
2018-07-10 18:09:51 -0400

    Generate pokemon table migration and add in schema

---
+++ server/models/migrations/20180710170452_create-pokemon-table.js
@@ -0,0 +1,61 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.hasTable('pokemon').then(exists => {
+    if (!exists) {
+      return knex.schema.createTable('pokemon', table => {
+        table.uuid('id').primary();
+        // want to confirm - primary keys are inherently notNullable
+        table
+          .integer('pokemonNumber')
+          .notNullable()
+          .unique();
+        table
+          .string('name')
+          .notNullable()
+          .unique();
+        table.integer('attack').notNullable();
+        table.integer('defense').notNullable();
+        table
+          .enu('pokeType', [
+            'normal',
+            'grass',
+            'fire',
+            'water',
+            'electric',
+            'psychic',
+            'ghost',
+            'dark',
+            'fairy',
+            'rock',
+            'ground',
+            'steel',
+            'flying',
+            'fighting',
+            'bug',
+            'ice',
+            'dragon',
+            'poison',
+          ])
+          .notNullable();
+        // are enums & json used because they are preferable to a join table?
+        table
+          .json('moves')
+          .defaultTo([])
+          .notNullable();
+        // what is the default an array here if entries are of type JSON?
+        table.string('imageUrl').notNullable();
+        table
+          .timestamp('createdAt')
+          .defaultTo(knex.raw('now()'))
+          .notNullable();
+        table
+          .timestamp('updatedAt')
+          .defaultTo(knex.raw('now()'))
+          .notNullable();
+        table.timestamp('deletedAt');
+        // why in Commons example are some timestamps nullable?
+      });
+    }
+  });
+};
+
+exports.down = function(knex, Promise) {};

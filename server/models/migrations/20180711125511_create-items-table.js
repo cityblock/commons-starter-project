081b1edeeff5127d8316f482d1163e6b8d8b641e
Alicia Underhill <alicia.underhill@gmail.com>
2018-07-11 13:32:20 -0400

    Remove uniqueness validation on pokemonId and seed database

--- server/models/migrations/20180711125511_create-items-table.js
+++ server/models/migrations/20180711125511_create-items-table.js
@@ -1,17 +1,15 @@
 exports.up = async function(knex, Promise) {
-  const exists = await knex.schema.hasTable('items');
+  const exists = await knex.schema.hasTable('item');
   if (!exists) {
-    return knex.schema.createTable('items', table => {
+    return knex.schema.createTable('item', table => {
       table.uuid('id').primary();
       table.string('name').notNullable();
       table
         .uuid('pokemonId')
         .references('id')
         .inTable('pokemon')
-        .notNullable()
-        .unique();
-      // should I index the foreign key?
-      // table.index('pokemonId');
+        .notNullable();
+      table.index('pokemonId');
       table.integer('price').notNullable();
       table.integer('happiness').notNullable();
       table.string('imageUrl').notNullable();
@@ -29,8 +27,8 @@ exports.up = async function(knex, Promise) {
 };
 
 exports.down = async function(knex, Promise) {
-  const exists = await knex.schema.hasTable('items');
+  const exists = await knex.schema.hasTable('item');
   if (exists) {
-    return knex.schema.dropTable('items');
+    return knex.schema.dropTable('item');
   }
 };

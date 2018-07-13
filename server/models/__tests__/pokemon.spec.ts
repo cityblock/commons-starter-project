4df01af30055500f5bbdc50982f2c5d24ffa37d7
Alicia Underhill <31083146+AliciaUnderhill@users.noreply.github.com>
2018-07-13 14:47:02 -0400

    Pokemon model & tests (#3)
    * Write pokemon model through create() method
    
    * Add pokemon edit method
    
    * Finish pokemon model and associated tests
    
    * Remove test file
    
    * Add TypeError comment on .create() moves

---
+++ server/models/__tests__/pokemon.spec.ts
@@ -0,0 +1,99 @@
+import { transaction, Transaction } from 'objection';
+import uuid from 'uuid/v4';
+import Pokemon from '../pokemon';
+
+interface ISetup {
+  pokemon: Pokemon;
+}
+
+async function setup(txn: Transaction): Promise<ISetup> {
+  const pokemon = await Pokemon.create(
+    {
+      pokemonNumber: 1111,
+      name: 'Newbie',
+      attack: 9,
+      defense: 20,
+      pokeType: 'bug',
+      moves: ['sit still', 'eat pizza'],
+      imageUrl: 'fakeimageURL',
+    },
+    txn,
+  );
+  return { pokemon };
+}
+
+describe('Pokemon Model', () => {
+  let txn = null as any;
+
+  beforeEach(async () => {
+    txn = await transaction.start(Pokemon.knex());
+  });
+
+  afterEach(async () => {
+    await txn.rollback();
+  });
+
+  describe('create', () => {
+    it('creates a pokemon', async () => {
+      const { pokemon } = await setup(txn);
+      expect(pokemon.name).toBe('Newbie');
+      expect(pokemon.defense).toBe(20);
+    });
+  });
+
+  describe('get', () => {
+    it('retrieves a pokemon', async () => {
+      const { pokemon } = await setup(txn);
+      const fetchedPokemon = await Pokemon.get(pokemon.id, txn);
+      expect(fetchedPokemon.name).toEqual('Newbie');
+    });
+
+    it('throws error message if pokemon does not exist', async () => {
+      const randomUUID = uuid();
+      await expect(Pokemon.get(randomUUID, txn)).rejects.toMatch(
+        `No such pokemon exists: ${randomUUID}`,
+      );
+    });
+  });
+
+  describe('getAll', () => {
+    it('retrieves all pokemon', async () => {
+      const { pokemon } = await setup(txn);
+      await Pokemon.create(
+        {
+          pokemonNumber: 1112,
+          name: 'Poke',
+          attack: 12,
+          defense: 19,
+          pokeType: 'dragon',
+          moves: ['fly', 'breathe fire'],
+          imageUrl: 'fakeimageURL2',
+        },
+        txn,
+      );
+
+      const allPokemon = await Pokemon.getAll(txn);
+      expect(allPokemon.length).toBe(2);
+      expect(allPokemon[0].id).toBe(pokemon.id);
+    });
+  });
+
+  describe('edit', () => {
+    it('correctly updates a pokemon', async () => {
+      const { pokemon } = await setup(txn);
+      const editedInput = {
+        name: 'Felix',
+      };
+      const editedPokemon = await Pokemon.edit(pokemon.id, editedInput, txn);
+      expect(editedPokemon.name).toEqual('Felix');
+    });
+  });
+
+  describe('delete', () => {
+    it('changes deletedAt from null', async () => {
+      const { pokemon } = await setup(txn);
+      const deletedPokemon = await Pokemon.delete(pokemon.id, txn);
+      expect(deletedPokemon.deletedAt).not.toBe(null);
+    });
+  });
+});
---
+++ server/models/pokemon.ts
@@ -0,0 +1,166 @@
+import { Model, Transaction } from 'objection';
+import uuid from 'uuid/v4';
+
+type PokeType =
+  | 'normal'
+  | 'grass'
+  | 'fire'
+  | 'water'
+  | 'electric'
+  | 'psychic'
+  | 'ghost'
+  | 'dark'
+  | 'fairy'
+  | 'rock'
+  | 'ground'
+  | 'steel'
+  | 'flying'
+  | 'fighting'
+  | 'bug'
+  | 'ice'
+  | 'dragon'
+  | 'poison';
+
+interface IPokemonCreateFields {
+  pokemonNumber: number;
+  name: string;
+  attack: number;
+  defense: number;
+  pokeType: PokeType;
+  moves: string[];
+  imageUrl: string;
+}
+
+interface IPokemonEditInput {
+  pokemonNumber?: number;
+  name?: string;
+  attack?: number;
+  defense?: number;
+  pokeType?: PokeType;
+  moves?: string[];
+  imageUrl?: string;
+}
+
+/* tslint:disable member-ordering */
+export default class Pokemon extends Model {
+  static modelPaths = [__dirname];
+  static pickJsonSchemaProperties = true;
+
+  id!: string;
+  pokemonNumber!: number;
+  name!: string;
+  attack!: number;
+  defense!: number;
+  pokeType!: PokeType;
+  moves!: string[];
+  imageUrl!: string;
+  createdAt!: string;
+  updatedAt!: string;
+  deletedAt!: string;
+
+  $beforeInsert() {
+    this.id = uuid();
+    this.createdAt = new Date().toISOString();
+    this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
+
+  static tableName = 'pokemon';
+
+  static jsonSchema = {
+    type: 'object',
+    properties: {
+      id: { type: 'string', format: 'uuid' },
+      pokemonNumber: { type: 'number' },
+      name: { type: 'string', minLength: 1 },
+      attack: { type: 'number' },
+      defense: { type: 'number' },
+      pokeType: {
+        type: 'string',
+        enum: [
+          'normal',
+          'grass',
+          'fire',
+          'water',
+          'electric',
+          'psychic',
+          'ghost',
+          'dark',
+          'fairy',
+          'rock',
+          'ground',
+          'steel',
+          'flying',
+          'fighting',
+          'bug',
+          'ice',
+          'dragon',
+          'poison',
+        ],
+      },
+      moves: { type: 'string' },
+      imageUrl: { type: 'string', minLength: 1 },
+      updatedAt: { type: 'string' },
+      createdAt: { type: 'string' },
+      deletedAt: { type: 'string' },
+    },
+    required: ['pokemonNumber', 'name', 'attack', 'defense', 'pokeType', 'moves', 'imageUrl'],
+  };
+
+  static async create(input: IPokemonCreateFields, txn: Transaction): Promise<Pokemon> {
+    const formattedInput = {
+      ...input,
+      // included type 'any' on moves below since TS expects an array of strings
+      // but DB validation expects JSON
+      moves: JSON.stringify(input.moves) as any,
+    };
+    return this.query(txn).insertAndFetch(formattedInput);
+  }
+
+  static async get(pokemonId: string, txn: Transaction): Promise<Pokemon> {
+    const pokemon = await this.query(txn).findOne({ id: pokemonId, deletedAt: null });
+    if (!pokemon) {
+      return Promise.reject(`No such pokemon exists: ${pokemonId}`);
+    }
+    return pokemon;
+  }
+
+  static async getAll(txn: Transaction): Promise<Pokemon[]> {
+    return this.query(txn)
+      .where({ deletedAt: null })
+      .orderBy('pokemonNumber', 'ASC');
+  }
+
+  static async edit(
+    pokemonId: string,
+    input: IPokemonEditInput,
+    txn: Transaction,
+  ): Promise<Pokemon> {
+    const formattedInput = {
+      ...input,
+      moves: JSON.stringify(input.moves) as any,
+    };
+    const updatedPokemon = await this.query(txn).patchAndFetchById(pokemonId, formattedInput);
+
+    if (!updatedPokemon) {
+      return Promise.reject(`No such pokemon exists: ${updatedPokemon}`);
+    }
+    return updatedPokemon;
+  }
+
+  static async delete(pokemonId: string, txn: Transaction): Promise<Pokemon> {
+    const deletedPokemon = await this.query(txn).patchAndFetchById(pokemonId, {
+      deletedAt: new Date().toISOString(),
+    });
+
+    if (!deletedPokemon) {
+      return Promise.reject(`No such pokemon: ${deletedPokemon}`);
+    }
+    return deletedPokemon;
+  }
+}
+
+/* tslint:enable member-ordering */

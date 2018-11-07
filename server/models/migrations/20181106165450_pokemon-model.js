0dfc8fa283dbbc54a85646fdf90596e068600cd4
Dan DeWald <dan@cityblock.com>
2018-11-07 12:52:10 -0500

    Add items migration and update code formatting for both migration files

--- server/models/migrations/20181106165450_pokemon-model.js
+++ server/models/migrations/20181106165450_pokemon-model.js
@@ -1,31 +1,12 @@
-exports.up = function (knex, Promise) {
-  return knex.schema.hasTable('pokemon').then(exists => {
+exports.up = (knex, Promise) => (
+  knex.schema.hasTable('pokemon').then(exists => {
     if (!exists) {
       return knex.schema.createTable('pokemon', table => {
-
-        table
-          .uuid('id')
-          .primary()
-          .notNullable();
-
-        table
-          .integer('pokemonNumber')
-          .notNullable()
-          .unique();
-
-        table
-          .string('name')
-          .notNullable()
-          .unique();
-
-        table
-          .integer('attack')
-          .notNullable();
-
-        table
-          .integer('defense')
-          .notNullable();
-
+        table.uuid('id').primary().notNullable();
+        table.integer('pokemonNumber').notNullable().unique();
+        table.string('name').notNullable().unique();
+        table.integer('attack').notNullable();
+        table.integer('defense').notNullable();
         table
           .enu('pokeType', [
             'normal',
@@ -48,36 +29,20 @@ exports.up = function (knex, Promise) {
             'poison'
           ])
           .notNullable();
-
-        table
-          .json('moves')
-          .notNullable()
-          .defaultTo('[]');
-
-        table
-          .string('imageUrl')
-          .notNullable();
-
-        table
-          .timestamp('createdAt')
-          .notNullable();
-
-        table
-          .timestamp('updatedAt')
-          .notNullable();
-
-        table
-          .timestamp('deletedAt')
-          .notNullable();
+        table.json('moves').notNullable().defaultTo('[]');
+        table.string('imageUrl').notNullable();
+        table.timestamp('createdAt').defaultTo(knex.raw('now()'));
+        table.timestamp('updatedAt').defaultTo(knex.raw('now()'));
+        table.timestamp('deletedAt').nullable();
       });
     }
-  });
-};
+  })
+);
 
-exports.down = function (knex, Promise) {
-  return knex.schema.hasTable('pokemon').then(exists => {
+exports.down = (knex, Promise) => (
+  knex.schema.hasTable('pokemon').then(exists => {
     if (exists) {
       return knex.schema.dropTable('pokemon');
     }
-  });
-};
+  })
+);

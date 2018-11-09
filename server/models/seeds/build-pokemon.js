41080452bc5b6c41a87b569b0ebfbaf0f8141f16
Dan DeWald <dan@cityblock.com>
2018-11-09 18:01:13 -0500

    Break seeding code architecture into separate modules

---
+++ server/models/seeds/build-pokemon.js
@@ -0,0 +1,22 @@
+const uuid = require('uuid');
+
+module.exports = (
+  name,
+  pokemonNumber,
+  attack,
+  defense,
+  pokeType,
+  moves,
+  imageUrl
+) => ({
+  id: uuid.v4(),
+  name,
+  pokemonNumber,
+  attack,
+  defense,
+  pokeType,
+  moves: JSON.stringify(moves),
+  imageUrl,
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+});

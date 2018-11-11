b802538dd3e5f787038cc92fa5b6f90514a25f77
Dan DeWald <dan@cityblock.com>
2018-11-11 18:24:17 -0500

    Write tests for Item#edit and Item#delete

--- server/models/item.ts
+++ server/models/item.ts
@@ -1,4 +1,5 @@
 import { Model, Transaction } from 'objection';
+import uuid from 'uuid/v4';
 
 export interface IItemCreateFields {
   id: string;
@@ -10,6 +11,19 @@ export interface IItemCreateFields {
   createdAt: string;
   updatedAt: string;
   deletedAt?: string;
+}
+
+export interface IItemEditInput {
+  id: string;
+  name?: string;
+  pokemonNumber?: number;
+  attack?: number;
+  defense?: number;
+  pokeType?: string;
+  moves?: string;
+  imageUrl?: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export default class Item extends Model {
@@ -34,6 +48,14 @@ export default class Item extends Model {
     if (!item) return Promise.reject(`item with id ${itemId} not found.`);
     return item;
   }
+
+  static async edit(itemId: string, fieldsToUpdate: IItemEditInput, txn: Transaction): Promise<Item> {
+    return this.query(txn).updateAndFetchById(itemId, fieldsToUpdate);
+  }
+
+  static async delete(itemId: string, txn: Transaction): Promise<Item> {
+    return this.query(txn).patchAndFetchById(itemId, { deletedAt: new Date().toISOString() });
+  }
   id!: string;
   name!: string;
   pokemonId!: string;
@@ -44,4 +66,14 @@ export default class Item extends Model {
   updatedAt!: string;
   deletedAt!: string | null;
   [k: string]: any;
+
+  $beforeInsert() {
+    this.id = uuid();
+    this.createdAt = new Date().toISOString();
+    this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
 }

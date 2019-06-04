09c68161c501dd142f85524d6ffc3ee7c95ec228
Kirill Levin <kirill.levin@cityblock.com>
2019-06-04 17:06:28 -0400

    Fix some weird import error

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ import { Model, Transaction } from 'objection';
 import pg from 'pg';
 import config from './config';
 import expressConfig from './express';
-import * as knexConfig from './models/knexfile';
+const knexConfig = require('./models/knexfile');
 
 const logger = console as any;
 

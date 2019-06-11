50a6bd7210c33038cfaea5a50421cd0136ae8608
aldasmallthings <alda.pontes@cityblock.com>
2019-06-11 10:19:52 -0400

    w

--- server/graphql/pokemon-resolver.ts
+++ server/graphql/pokemon-resolver.ts
@@ -1,7 +1,7 @@
-import { transaction, Transaction } from 'objection';
-import { IRootQueryType } from 'schema';
-import Pokemon from '../models/pokemon';
-import { IContext } from './shared/utils';
+// import { transaction, Transaction } from 'objection';
+// import { IRootQueryType } from 'schema';
+// import Pokemon from '../models/pokemon';
+// import { IContext } from './shared/utils';
 
 // two types of resolving: one is telling the query which data model method to run
 // other one is
@@ -11,12 +11,12 @@ export interface IQuery {
   pokemonId: string;
 }
 
-export async function resolvePokemon(
-  root: any,
-  { pokemonId }: IQuery,
-  { testTransaction, getOrCreateTransaction }: IContext,
-): Promise<IRootQueryType['pokemon']> {
-  return getOrCreateTransaction(testTransaction, async txn => {
-    return Pokemon.get(pokemonId, txn);
-  });
-}
+// export async function resolvePokemon(
+//   root: any,
+//   { pokemonId }: IQuery,
+//   { testTransaction, getOrCreateTransaction }: IContext,
+// ): Promise<IRootQueryType['pokemon']> {
+//   return getOrCreateTransaction(testTransaction, async txn => {
+//     return Pokemon.get(pokemonId, txn);
+//   });
+// }

11fd64ff1e5bd2e1b8bb12da40d52dd86f3cd17e
Jay Bhagat <jay.bhagat@cityblock.com>
2019-07-22 20:04:47 -0400

    get and create tests written and passing

--- server/graphql/__tests__/item-resolver.spec.ts
+++ server/graphql/__tests__/item-resolver.spec.ts
@@ -1,17 +1,19 @@
 import { graphql, print } from 'graphql';
 import { cloneDeep } from 'lodash';
 import { transaction } from 'objection';
-// import createItem from '../../../app/graphql/queries/create-item-mutation.graphql';
+import { PokeType } from 'schema';
+import createItem from '../../../app/graphql/queries/create-item-mutation.graphql';
 // import deleteItem from '../../../app/graphql/queries/delete-item-mutation.graphql';
 // import editItem from '../../../app/graphql/queries/edit-item-mutation.graphql';
 import getItem from '../../../app/graphql/queries/get-item-query.graphql';
 import { setupDb } from '../../lib/test-utils';
 import Item from '../../models/item';
+import Pokemon from '../../models/pokemon';
 import schema from '../make-executable-schema';
 
 describe('item resolvers', () => {
   const getItemQuery = print(getItem);
-  // const createItemMutation = print(createItem);
+  const createItemMutation = print(createItem);
   // const editItemMutation = print(editItem);
   // const deleteItemMutation = print(deleteItem);
 
@@ -59,29 +61,40 @@ describe('item resolvers', () => {
       expect(cloneDeep(result.data!.singleItem)).toMatchObject(randomItemToMatchGQLQuery);
     });
   });
-  // describe('#create', () => {
-  //   it('should create a pokemon', async () => {
-  //     const jaimon = {
-  //       name: 'Jason Derulo',
-  //       pokemonNumber: 1001,
-  //       attack: 9001,
-  //       defense: 100,
-  //       pokeType: 'dragon',
-  //       moves: ['Electric Slide', 'Ali Shuffle'],
-  //       imageUrl: 'cityblock.com',
-  //     };
-  //     const result = await graphql(
-  //       schema,
-  //       createAPokemonMutation,
-  //       null,
-  //       {
-  //         testTransaction: txn,
-  //       },
-  //       { ...jaimon },
-  //     );
-  //     expect(cloneDeep(result.data!.createPokemon.name)).toEqual(jaimon.name);
-  //   });
-  // });
+  describe('#create', () => {
+    it('should create a item', async () => {
+      const jaimon = await Pokemon.create(
+        {
+          name: 'Jason Derulo',
+          pokemonNumber: 1001,
+          attack: 9001,
+          defense: 100,
+          pokeType: 'dragon' as PokeType,
+          moves: ['Electric Slide', 'Ali Shuffle'],
+          imageUrl: 'cityblock.com',
+        },
+        txn,
+      );
+      const itemForJaimon = {
+        name: 'Boxing Gloves',
+        pokemonId: jaimon.id,
+        price: 9001,
+        happiness: 100,
+        imageUrl: 'cityblock.com',
+      };
+      const result = await graphql(
+        schema,
+        createItemMutation,
+        null,
+        {
+          testTransaction: txn,
+        },
+        { ...itemForJaimon },
+      );
+      expect(cloneDeep(result.data!.createItem.pokemonId)).toEqual(itemForJaimon.pokemonId);
+      expect(cloneDeep(result.data!.createItem.name)).toEqual(itemForJaimon.name);
+    });
+  });
   // describe('#edit', () => {
   //   it('should edit a pokemon', async () => {
   //     const testingPokemonArr = await Pokemon.getAll(txn);

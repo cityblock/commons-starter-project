a3e8cbcd14eb702cfff2a8ff3d3b731cef93f8bc
Aster Ryan <aster.ryan@cityblock.com>
2019-07-25 12:57:40 -0400

    Add getOrCreateTransaction to our context

--- server/middleware/graphql-middleware.ts
+++ server/middleware/graphql-middleware.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-express';
 import { Transaction } from 'objection';
 import schema from '../graphql/make-executable-schema';
+import { getOrCreateTransaction } from '../graphql/shared/utils';
 
 /**
  *  An unorthodx setup for Apollo Server Express
@@ -10,6 +11,7 @@ import schema from '../graphql/make-executable-schema';
 export const graphqlMiddleware = async (existingTxn?: Transaction) => {
   return new ApolloServer({
     schema,
+    context: { getOrCreateTransaction },
     debug: false,
     tracing: true,
     cacheControl: true,

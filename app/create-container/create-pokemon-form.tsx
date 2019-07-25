3defcd509956dbc4735c0f60230c23352d2d7de2
Jay Bhagat <jay.bhagat@cityblock.com>
2019-07-25 17:00:42 -0400

    apollo hooked up and mutations is hooked up and visible in devtools

--- app/create-container/create-pokemon-form.tsx
+++ app/create-container/create-pokemon-form.tsx
@@ -1,25 +1,53 @@
 import { ApolloError } from 'apollo-client';
 import { pokemonCreate_createPokemon } from 'app/graphql/types';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 import pokemonCreate from '../graphql/queries/create-pokemon-mutation.graphql';
 import { pokeType } from './form-logic';
 
-interface IRouteProps {
-  match: { params: { pokemonId: string } };
+enum PokeType {
+  bug = 'bug',
+  dark = 'dark',
+  dragon = 'dragon',
+  electric = 'electric',
+  fairy = 'fairy',
+  fighting = 'fighting',
+  fire = 'fire',
+  flying = 'flying',
+  ghost = 'ghost',
+  grass = 'grass',
+  ground = 'ground',
+  ice = 'ice',
+  normal = 'normal',
+  poison = 'poison',
+  psychic = 'psychic',
+  rock = 'rock',
+  steel = 'steel',
+  water = 'water',
+}
+
+interface IPokemon {
+  id: string;
+  name: string;
+  pokemonNumber: number;
+  pokeType: PokeType;
+  attack: number;
+  defense: number;
+  moves: string[];
+  imageUrl: string;
 }
 
 interface IGraphqlProps {
-  createPokemon: pokemonCreate_createPokemon;
-  loading: boolean;
-  error: ApolloError | null | undefined;
+  createPokemonMutation: (options: {
+    variables: pokemonCreate_createPokemon;
+  }) => { data: pokemonCreate_createPokemon; errors: ApolloError[] };
 }
 
-const initialPokemon = {
+const initialPokemon: IPokemon = {
   id: '',
   name: '',
   pokemonNumber: 0,
-  pokeType: 'grass',
+  pokeType: PokeType.grass,
   attack: 100,
   defense: 100,
   moves: ['Cha Cha'],
@@ -29,59 +57,63 @@ const initialPokemon = {
 export const CreatePokemonForm: React.StatelessComponent<IGraphqlProps> = (
   props: IGraphqlProps,
 ) => {
-  const { createPokemon, loading, error } = props;
-  const [pokemon, setPokemon] = useState(initialPokemon);
+  const { createPokemonMutation } = props;
+  const [pokemon, setPokemon] = useState<IPokemon>(initialPokemon);
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>,
+  ) => {
+    const fieldToChange = e.target.name;
+    const valueToSet = e.target.value;
+    const newPokemon = { ...pokemon, [fieldToChange]: valueToSet };
+    setPokemon(newPokemon);
+  };
+  const submit = async e => {
+    e.preventDefault();
+    await createPokemonMutation({ variables: pokemon });
+  };
   return (
     <>
-      {loading && <p>Loading up!</p>}
-      {error && (
-        <React.Fragment>
-          <img src="../test-container/desus-mero.jpg" />
-          <h1>Fix ya face, kehd</h1>
-        </React.Fragment>
-      )}
-      {!loading && (
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            console.log('pib');
-          }}
-        >
-          <h1>Create a pokemon</h1>
-          <br />
-          <label>Name:</label>
-          <input type="text" name="name" value={pokemon.name} />
-          <br />
-          <label>Pokemon Number:</label>
-          <input type="text" name="pokemonNumber" value={pokemon.pokemonNumber} />
-          <br />
-          <label>Pokemon Type:</label>
-          <select name="pokeType" value={pokemon.pokeType}>
-            {pokeType.map(type => {
-              return <option key={type}>{type}</option>;
-            })}
-          </select>
-          <br />
-          <label>Attack:</label>
-          <input type="text" name="attack" value={pokemon.attack} />
-          <br />
-          <label>Defense:</label>
-          <input type="text" name="defense" value={pokemon.defense} />
-          <br />
-          <label>Moves:</label>
-          <textarea rows={4} cols={50} name="moves" value={pokemon.moves} />
-          <br />
-          <label>Image URL:</label>
-          <input type="text" name="imageUrl" value={pokemon.imageUrl} />
-          <br />
-          <input type="submit" value="Pib" />
-        </form>
-      )}
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          submit(e);
+        }}
+      >
+        <h1>Create a pokemon</h1>
+        <br />
+        <label>Name:</label>
+        <input type="text" name="name" value={pokemon.name} onChange={onChange} />
+        <br />
+        <label>Pokemon Number:</label>
+        <input type="text" name="pokemonNumber" value={pokemon.pokemonNumber} onChange={onChange} />
+        <br />
+        <label>Pokemon Type:</label>
+        <select name="pokeType" value={PokeType.grass} onChange={onChange}>
+          {pokeType.map(type => {
+            return <option key={type}>{type}</option>;
+          })}
+        </select>
+        <br />
+        <label>Attack:</label>
+        <input type="text" name="attack" value={pokemon.attack} onChange={onChange} />
+        <br />
+        <label>Defense:</label>
+        <input type="text" name="defense" value={pokemon.defense} onChange={onChange} />
+        <br />
+        <label>Moves:</label>
+        <textarea rows={4} cols={50} name="moves" value={pokemon.moves} onChange={onChange} />
+        <br />
+        <label>Image URL:</label>
+        <input type="text" name="imageUrl" value={pokemon.imageUrl} onChange={onChange} />
+        <br />
+        <input type="submit" value="Pib" />
+      </form>
     </>
   );
 };
 
 export default graphql(pokemonCreate, {
+  name: 'createPokemonMutation',
   options: (props: pokemonCreate_createPokemon) => {
     return {
       variables: {
@@ -96,9 +128,4 @@ export default graphql(pokemonCreate, {
       },
     };
   },
-  props: ({ data }) => ({
-    loading: data ? data.loading : false,
-    error: data ? data.error : null,
-    createPokemon: data ? (data as any).createPokemon : null,
-  }),
 })(CreatePokemonForm);

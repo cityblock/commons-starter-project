3a54103daf6ed637e04a9000d9edca3866ed3df4
Lauren Friedmann <lauren.friedmann@cityblock.com>
2019-10-01 15:38:25 -0400

    create pokemon postgres migration

---
+++ server/models/migrations/20191001142711_create-pokemon-table.js
@@ -0,0 +1,66 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.hasTable('pokemon').then(exists => {
+        if (!exists) {
+            return knex.schema.createTable('pokemon', table => {
+                table
+                    .uuid('id')
+                    .notNullable()
+                    .unique()
+                    .primary();
+                table
+                    .integer('pokemonNumber')
+                    .unique()
+                    .notNullable();
+                table
+                    .string('name')
+                    .unique()
+                    .notNullable();
+                table.integer('attack').notNullable();
+                table.integer('defense').notNullable();
+                table
+                    .enu('pokeType', [
+                        'normal',
+                        'grass',
+                        'fire',
+                        'water',
+                        'electric',
+                        'psychic',
+                        'ghost',
+                        'fairy',
+                        'rock',
+                        'ground',
+                        'steel',
+                        'flying',
+                        'fighting',
+                        'bug',
+                        'ice',
+                        'dragon',
+                        'poison',
+                    ])
+                    .notNullable();
+                table
+                    .json('moves')
+                    .notNullable()
+                    .defaultTo('[]');
+                table.string('imageUrl').notNullable();
+                table
+                    .timestamp('createdAt')
+                    .notNullable()
+                    .defaultTo(knex.raw('now()'));
+                table
+                    .timestamp('updatedAt')
+                    .notNullable()
+                    .defaultTo(knex.raw('now()'));
+                table.timestamp('deletedAt').notNullable();
+            });
+        }
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.hasTable('pokemon').then(exists => {
+        if (exists) {
+            return knex.schema.dropTable('pokemon');
+        }
+    });
+};

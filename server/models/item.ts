ff96ec7f86b3dba84fd631bb4ce6d30390e060da
Lauren Friedmann <lauren.friedmann@cityblock.com>
2019-10-02 15:56:27 -0400

    add item and pokemon data models

---
+++ server/models/item.ts
@@ -0,0 +1,111 @@
+import { Model, RelationMappings, Transaction } from 'objection';
+import Pokemon from './pokemon';
+
+export interface IItemCreateInput {
+  pokemonId: string;
+  price: number;
+  happiness: number;
+  imageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IItemEditInput {
+  pokemonId?: string;
+  price?: number;
+  happiness?: number;
+  imageUrl?: string;
+}
+
+export default class Item extends Model {
+  static get relationMappings(): RelationMappings {
+    return {
+      pokemon: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Pokemon,
+        join: {
+          from: 'item.pokemonId',
+          to: 'pokemon.id',
+        },
+      },
+    };
+  }
+  static modelPaths = [__dirname];
+  static pickJsonSchemaProperties = true;
+
+  static tableName = 'item';
+
+  static jsonSchema = {
+    type: 'object',
+    properties: {
+      id: { type: 'string' },
+      pokemonId: { type: 'string' },
+      price: { type: 'number' },
+      happiness: { type: 'number' },
+      imageUrl: { type: 'string' },
+      createdAt: { type: 'string' },
+      updatedAt: { type: 'string' },
+      deletedAt: { type: ['string', 'null'] },
+    },
+    required: [
+      'id',
+      'pokemonId',
+      'price',
+      'happiness',
+      'imageUrl',
+      'imageUrl',
+      'createdAt',
+      'updatedAt',
+    ],
+  };
+
+  static async get(itemId: string, txn: Transaction): Promise<Item> {
+    const pokemon = await this.query(txn).findById(itemId);
+    if (pokemon) {
+      return pokemon as any;
+    }
+    return Promise.reject(`Could not find an item with id: ${itemId}`);
+  }
+
+  static async create(input: IItemCreateInput, txn: Transaction): Promise<Item> {
+    return this.query(txn).insertAndFetch(input);
+  }
+
+  static async edit(itemId: string, item: IItemEditInput, txn: Transaction): Promise<Item> {
+    const itemExists = await this.query(txn).findById(itemId);
+    if (!itemExists) {
+      return Promise.reject(`Can't find an existing item to edit with id: ${itemId}`);
+    }
+    return this.query(txn).patchAndFetchById(itemId, item);
+  }
+
+  static async delete(itemId: string, txn: Transaction): Promise<Item> {
+    const itemExists = await this.query(txn)
+      .findById(itemId)
+      .where({ deletedAt: null });
+    if (!itemExists) {
+      return Promise.reject(
+        `Can't find an existing item with id, or it has already been deleted: ${itemId}`,
+      );
+    }
+    return this.query(txn).patchAndFetchById(itemId, { deletedAt: new Date().toISOString() });
+  }
+
+  id!: string;
+  pokemonId!: string;
+  price!: number;
+  happiness!: number;
+  imageUrl!: string;
+  createdAt!: string;
+  updatedAt!: string;
+  deletedAt!: string | null;
+
+  $beforeInsert() {
+    this.createdAt = new Date().toISOString();
+    this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
+}
--- server/models/pokemon.ts
+++ server/models/pokemon.ts
@@ -1,7 +1,41 @@
-import { Model, Transaction } from 'objection';
-import { number } from 'prop-types';
+import { Model, RelationMappings, Transaction } from 'objection';
+import Item from './item';
+
+export interface IPokemonCreateInput {
+  pokemonNumber: number;
+  name: string;
+  attack: number;
+  defense: number;
+  pokeType: string;
+  moves: JSON;
+  imageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IPokemonEditInput {
+  pokemonNumber?: number;
+  name?: string;
+  attack?: number;
+  defense?: number;
+  pokeType?: string;
+  moves?: JSON;
+  imageUrl?: string;
+}
 
 export default class Pokemon extends Model {
+  static get relationMappings(): RelationMappings {
+    return {
+      items: {
+        relation: Model.HasManyRelation,
+        modelClass: Item,
+        join: {
+          from: 'pokemon.id',
+          to: 'item.pokemonId',
+        },
+      },
+    };
+  }
   static modelPaths = [__dirname];
   static pickJsonSchemaProperties = true;
 
@@ -36,6 +70,54 @@ export default class Pokemon extends Model {
     ],
   };
 
+  static async getAll(txn: Transaction): Promise<Pokemon[]> {
+    return this.query(txn).orderBy('pokemonNumber');
+  }
+
+  static async get(pokemonId: string, txn: Transaction): Promise<Pokemon> {
+    const pokemon = await this.query(txn)
+      .findById(pokemonId)
+      .joinRelation('items');
+    if (pokemon) {
+      return pokemon as any;
+    }
+    return Promise.reject(`Could not find a pokemon with id: ${pokemonId}`);
+  }
+
+  static async create(input: IPokemonCreateInput, txn: Transaction): Promise<Pokemon> {
+    const pokemonExists = this.query(txn)
+      .where({ pokemonNumber: input.pokemonNumber })
+      .where({ deletedAt: null });
+    if (pokemonExists) {
+      return Promise.reject(
+        `Found an existing pokemon with pokemon number: ${input.pokemonNumber}`,
+      );
+    }
+    return this.query(txn).insertAndFetch(input);
+  }
+
+  static async edit(
+    pokemonId: string,
+    pokemon: IPokemonEditInput,
+    txn: Transaction,
+  ): Promise<Pokemon> {
+    const pokemonExists = await this.query(txn).findById(pokemonId);
+    if (!pokemonExists) {
+      return Promise.reject(`Can't find an existing pokemon with id: ${pokemonId}`);
+    }
+    return this.query(txn).patchAndFetchById(pokemonId, pokemon);
+  }
+
+  static async delete(pokemonId: string, txn: Transaction): Promise<Pokemon> {
+    const pokemonExists = await this.query(txn)
+      .findById(pokemonId)
+      .where({ deletedAt: null });
+    if (!pokemonExists) {
+      return Promise.reject(`Can't find an existing pokemon with id: ${pokemonId}`);
+    }
+    return this.query(txn).patchAndFetchById(pokemonId, { deletedAt: new Date().toISOString() });
+  }
+
   id!: string;
   pokemonNumber!: number;
   name!: string;
@@ -47,4 +129,13 @@ export default class Pokemon extends Model {
   createdAt!: string;
   updatedAt!: string;
   deletedAt!: string | null;
+
+  $beforeInsert() {
+    this.createdAt = new Date().toISOString();
+    this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
 }

73184afd7c48f255f04b109c57833f0234745a8c
Diana Stan <dianamaria4589@gmail.com>
2019-10-03 18:40:02 -0400

    initial commit for part 2: should probably rewrite some pokemon tests

---
+++ server/models/item.ts
@@ -0,0 +1,114 @@
+const { Model } = require('objection');
+import { Transaction } from 'objection';
+// import Pokemon from './pokemon';
+
+interface IItemCreateInput {
+  id: string;
+  name: string;
+  pokemonId: string;
+  price: number;
+  happiness: number;
+  imageUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+interface IItemEditInput {
+  price?: number;
+  happiness?: number;
+  imageUrl?: string;
+  updatedAt?: Date;
+  deletedAt?: Date | null;
+}
+
+export class Item extends Model {
+  static get tableName() {
+    return 'item';
+  }
+
+  static jsonSchema = {
+    type: 'object',
+    properties: {
+      id: { type: 'string', format: 'uuid' },
+      name: { type: 'string' },
+      pokemonId: { type: 'string' },
+      price: { type: 'number' },
+      happines: { type: 'number' },
+      imageUrl: { type: 'string' },
+      createdAt: { type: 'Date' },
+      updatedAt: { type: 'Date' },
+      deletedAt: { type: 'Date' },
+    },
+    required: [
+      'id',
+      'name',
+      'pokemonId',
+      'price',
+      'happiness',
+      'imageUrl',
+      'createdAt',
+      'updatedAt',
+    ],
+  };
+
+  static get relationshipMappings() {
+    const Pokemon = require('./pokemon');
+    return {
+      pokemon: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Pokemon,
+        join: {
+          from: 'item.pokemonId',
+          to: 'pokemon.id',
+        },
+      },
+    };
+  }
+
+  static async get(itemId: string, txn: Transaction): Promise<Item> {
+    const item = this.query().findOne({ id: itemId });
+    if (!item) {
+      return Promise.reject(`No such item: ${itemId}`);
+    }
+    return item;
+  }
+
+  static async getAll(txn: Transaction): Promise<Item[]> {
+    return this.query(txn);
+  }
+
+  static async create(input: IItemCreateInput, txn: Transaction): Promise<Item> {
+    const createdItem = this.query(txn).insertAndFetch(input);
+    if (!createdItem) {
+      return Promise.reject(`Could not create Item, please try again.`);
+    }
+    return createdItem;
+  }
+
+  static async edit(itemId: string, item: IItemEditInput, txn: Transaction): Promise<Item> {
+    const editedItem = this.query(txn).patchAndFetchById(itemId, item);
+    if (!editedItem) {
+      return Promise.reject(`No such item: ${itemId}`);
+    }
+    return editedItem;
+  }
+
+  static async delete(itemId: string, txn: Transaction): Promise<Item> {
+    await this.query(txn)
+      .where({ id: itemId, deletedAt: null })
+      .patch({ deletedAt: new Date() });
+
+    const deletedItem = this.query(txn).findById(itemId);
+    if (!deletedItem) {
+      return Promise.reject(`No such item: ${itemId}`);
+    }
+    return deletedItem;
+  }
+
+  static async getNonDeletedItem(txn: Transaction): Promise<Item> {
+    return this.query(txn)
+      .where({ deletedAt: null })
+      .limit(1);
+  }
+}
---
+++ server/models/tests/pokemon.spec.ts
@@ -0,0 +1,133 @@
+import { Transaction } from 'objection';
+import uuid from 'uuid/v4';
+import { Pokemon } from '../pokemon';
+import { all } from 'q';
+
+describe('get all pokemon', () => {
+  const txn = null as any;
+  it('should get all pokemon in ascending number order', async () => {
+    const allPokemon = await Pokemon.getAll(txn);
+    const firstPokemon = allPokemon[0];
+    expect(firstPokemon).toMatchObject({
+      id: 'f9f7c348-9ab2-43c4-8927-4f15e818bb24',
+      pokemonNumber: 1,
+      name: 'Bulbasaur',
+      attack: 11,
+      defense: 22,
+      pokeType: 'grass',
+      moves: ['Tackle', 'Growl', 'Leech Seed'],
+      imageUrl:
+        'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/1200px-001Bulbasaur.png',
+      createdAt: new Date('2019-10-02 12:43:03.856-04'),
+      updatedAt: new Date('2019-10-02 12:43:03.856-04'),
+      deletedAt: new Date('2019-10-03 17:38:34.155-04'),
+    });
+  });
+});
+
+describe('create new pokemon', () => {
+  const txn = null as any;
+  it('should create a new pokemon', async () => {
+    const currentTime = new Date(Date.now());
+    const pokemonUUID = uuid();
+    const pokeNumber = Math.floor(Math.random() * 10000);
+    const pokemonName = 'Test' + pokeNumber.toString();
+    const newPokemon = await Pokemon.create(
+      {
+        id: pokemonUUID,
+        pokemonNumber: pokeNumber,
+        name: pokemonName,
+        attack: 33,
+        defense: 15,
+        pokeType: 'psychic',
+        moves: ['Growl'],
+        imageUrl: 'https://www.cityblock.com/',
+        createdAt: currentTime,
+        updatedAt: currentTime,
+        deletedAt: null,
+      },
+      txn,
+    );
+    expect(newPokemon).toMatchObject({
+      id: pokemonUUID,
+      pokemonNumber: pokeNumber,
+      name: pokemonName,
+      attack: 33,
+      defense: 15,
+      pokeType: 'psychic',
+      moves: ['Growl'],
+      imageUrl: 'https://www.cityblock.com/',
+      createdAt: currentTime,
+      updatedAt: currentTime,
+    });
+  });
+});
+
+describe('get items for one pokemon', () => {
+  let txn = null as any;
+  it('should get all items for a pokemon', async () => {
+    const allPokemonAndItems = await Pokemon.get('04ccdadd-e156-42d8-9dd9-0a0e4fd760b0', txn);
+    expect(allPokemonAndItems[1].length).toEqual(3);
+  });
+});
+
+describe('edit a pokemon', () => {
+  let txn = null as any;
+  it('should edit a pokemon', async () => {
+    const allPokemon = await Pokemon.getAll(txn);
+    const randomInt = Math.floor(Math.random() * allPokemon.length);
+    const randomPokemon = allPokemon[randomInt];
+    const currentTime = new Date(Date.now());
+    const fieldToEditValue = randomPokemon.attack + 1;
+    const editedPokemon = await Pokemon.edit(
+      randomPokemon.id,
+      {
+        attack: fieldToEditValue,
+        updatedAt: currentTime,
+      },
+      txn,
+    );
+    expect(editedPokemon).toMatchObject({
+      id: randomPokemon.id,
+      pokemonNumber: randomPokemon.pokemonNumber,
+      name: randomPokemon.name,
+      attack: fieldToEditValue,
+      defense: randomPokemon.defense,
+      pokeType: randomPokemon.pokeType,
+      moves: randomPokemon.moves,
+      imageUrl: randomPokemon.imageUrl,
+      createdAt: randomPokemon.createdAt,
+      updatedAt: currentTime,
+      deletedAt: randomPokemon.deletedAt,
+    });
+  });
+});
+
+describe('test soft delete', () => {
+  let txn = null as any;
+  it('should soft delete a specific pokemon', async () => {
+    const itemToDeleteList = await Pokemon.getNonDeletedPokemon(txn);
+    const itemToDelete = itemToDeleteList[0];
+    expect(itemToDelete.deletedAt).toBeNull();
+    const deletedItem = await Pokemon.delete(itemToDelete.id, txn);
+    expect(deletedItem.deletedAt).toBeTruthy();
+  });
+});
+
+describe('get pokemon and items for specific pokemon id', () => {
+  let txn = null as any;
+  it('should get a pokemon and all of its items', async () => {
+    const testPokemon = await Pokemon.get('04ccdadd-e156-42d8-9dd9-0a0e4fd760b0', txn);
+    expect(testPokemon).toMatchObject({
+      id: '04ccdadd-e156-42d8-9dd9-0a0e4fd760b0',
+      name: 'Pokemon Egg',
+      pokemonId: 'c818bf28-d0b4-4522-b326-a440ad0ceb3c',
+      price: 69,
+      happiness: 73,
+      imageUrl: 'https://rebekahlang.files.wordpress.com/2015/08/pokemon-egg-png.png',
+      createdAt: new Date('2019-10-02 12:43:03.909-04'),
+      updatedAt: new Date('2019-10-02 12:43:03.909-04'),
+      deletedAt: null,
+    });
+  });
+});

1034062bf7f3d22e88e0b434ee7c8aebff8873c7
Dan Hopkins <dan-hopkins@github.com>
2020-01-29 13:52:46 -0500

    Initial db migrations (pokemon and item tables)

---
+++ server/models/migrations/20200129120602_item-init.ts
@@ -0,0 +1,28 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.hasTable('item').then(exists => {
+    if (!exists) {
+      return knex.schema.createTable('item', table => {
+        table.uuid('id').primary().unique().notNullable();
+        table.string('name').notNullable();
+        table.uuid('pokemonId').references('id').inTable('pokemon').notNullable();
+        table.integer('price').notNullable();
+        table.integer('happiness').notNullable();
+        table.string('imageUrl').notNullable();
+        table.timestamp('createdAt').defaultTo(knex.raw('now()'));
+        table.timestamp('updatedAt').defaultTo(knex.raw('now()'));
+        table.timestamp('deletedAt').nullable();
+      });
+    }
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.hasTable('item').then(exists => {
+    if (exists) {
+      return knex.schema.dropTable('item');
+    }
+  });
+}

8d505bee8867732f5ae81993fc10089832415f6e
Dan Hopkins <dan-hopkins@github.com>
2020-02-13 10:37:44 -0500

    Render all Pokemon in left rail component

--- app/main-container/main-container.tsx
+++ app/main-container/main-container.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { compose, graphql } from 'react-apollo';
+import getPokemonsQuery from '../graphql/get-pokemons.graphql';
+import { getPokemons } from '../graphql/types';
 import styles from './css/main.css';
 
-export const MainContainer: React.StatelessComponent = () => {
+
+interface IGraphqlProps {
+  pokemons?: getPokemons['pokemons'],
+}
+
+export const MainContainer: React.StatelessComponent<IGraphqlProps> = (props: IGraphqlProps) => {
+  if (!props.pokemons) return null;
+
   return (
     <div className={styles.body}>
-      <h1>Your Project here!</h1>
+      <ul>
+        {props.pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default MainContainer;
+export default compose(
+  graphql(getPokemonsQuery, {
+    options: { fetchPolicy: 'network-only' },
+    props: ({ data }) => ({
+      pokemons: data ? (data as any).pokemons : null,
+    }),
+  }),
+)(MainContainer) as React.ComponentClass<IGraphqlProps>;

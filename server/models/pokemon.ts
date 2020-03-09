e49a68513119d3dfc489787021de51a9d9f2213c
jasmine zangi <jasmine.zangi@cityblock.com>
2020-03-09 16:54:07 -0400

    added create api and test

--- server/models/pokemon.ts
+++ server/models/pokemon.ts
@@ -1,70 +1,46 @@
 import { JsonSchema, Model, QueryBuilder, RelationMappings, Transaction } from 'objection';
 
-enum PokemonType {
-  normal = 'normal',
-  grass = 'grass',
-  fire = 'fire',
-  water = 'water',
-  electric = 'electric',
-  psychic = 'psychic',
-  ghost = 'ghost',
-  dark = 'dark',
-  fairy = 'fairy',
-  rock = 'rock',
-  ground = 'ground',
-  steel = 'steel',
-  flying = 'flying',
-  fighting = 'fighting',
-  bug = 'bug',
-  ice = 'ice',
-  dragon = 'dragon',
-  poison = 'poison'
-}
+// enum PokemonType {
+//   normal = 'normal',
+//   grass = 'grass',
+//   fire = 'fire',
+//   water = 'water',
+//   electric = 'electric',
+//   psychic = 'psychic',
+//   ghost = 'ghost',
+//   dark = 'dark',
+//   fairy = 'fairy',
+//   rock = 'rock',
+//   ground = 'ground',
+//   steel = 'steel',
+//   flying = 'flying',
+//   fighting = 'fighting',
+//   bug = 'bug',
+//   ice = 'ice',
+//   dragon = 'dragon',
+//   poison = 'poison'
+// }
 
 export interface ITest {
-  id: string
+  id: string;
 }
 export interface IPokemonCreateFields {
-  pokemonId: string
-  pokemonNumber: number
-  name: string
-  attack: number
-  defense: number
-  pokeType: string
-  moves: string[]
-  imageUrl: string
+  pokemonId: string;
+  pokemonNumber: number;
+  name: string;
+  attack: number;
+  defense: number;
+  pokeType: string;
+  moves: string[];
+  imageUrl: string;
 }
 
 export default class Pokemon extends Model {
-  //static modelPaths = [__dirname]
+  // static modelPaths = [__dirname]
   static pickJsonSchemaProperties = true;
   static tableName = 'pokemon';
 
-  id!: string
-  pokemonNumber!: number
-  name!: string
-  attack!: number
-  defense!: number
-  pokeType!: string
-  moves!: string[]
-  imageUrl!: string
-  createdAt!: string;
-  updatedAt!: string;
-  deletedAt!: string | null;
-
-  $beforeInsert() {
-    this.createdAt = new Date().toISOString();
-    this.updatedAt = new Date().toISOString();
-  }
-
-  $beforeUpdate() {
-    this.updatedAt = new Date().toISOString();
-  }
-
-  $beforeDelete() {
-    this.deletedAt = new Date().toISOString()
-  }
-
+  // tslint:disable-next-line: member-ordering
   static jsonSchema = {
     type: 'object',
     properties: {
@@ -79,16 +55,41 @@ export default class Pokemon extends Model {
       createdAt: { type: 'string' },
       updatedAt: { type: 'string' },
       deletedAt: { type: ['string', 'null'] },
-
     },
     required: ['pokemonNumber', 'name', 'attack', 'defense', 'pokeType', 'moves', 'imageUrl'],
-  }
+  };
 
+  // tslint:disable-next-line: member-ordering
   static async create(pokemon: IPokemonCreateFields, txn: Transaction) {
-    return this.query(txn).insertAndFetch(pokemon)
+    return this.query(txn).insertAndFetch(pokemon);
   }
 
   static async getAll(txn: Transaction) {
-    return await this.query(txn).orderBy('pokemonNumber')
+    return this.query(txn).orderBy('pokemonNumber');
+  }
+
+  id!: string;
+  pokemonNumber!: number;
+  name!: string;
+  attack!: number;
+  defense!: number;
+  pokeType!: string;
+  moves!: string[];
+  imageUrl!: string;
+  createdAt!: string;
+  updatedAt!: string;
+  deletedAt!: string | null;
+
+  $beforeInsert() {
+    this.createdAt = new Date().toISOString();
+    this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updatedAt = new Date().toISOString();
+  }
+
+  $beforeDelete() {
+    this.deletedAt = new Date().toISOString();
   }
 }
